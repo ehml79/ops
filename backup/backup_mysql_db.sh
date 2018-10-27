ec69a79420ad4bfd92271b86970d64d0763bbfb5
ehml79 <ehml79@foxmail.com>
2018-10-27 23:01:27 +0800

    update backup_mysql_db.sh

--- backup/backup_mysql_db.sh
+++ backup/backup_mysql_db.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # vars
-#export MYSQL_PWD=$(cat /data/save/mysql_root)
 # 备份mysql数据脚本
 
 date=$(date +%F)
@@ -9,7 +8,9 @@ time=$(date +%H-%M-%S)
 backup_dir=/data/backup/database/${date}/${time}
 backup_log=/data/backup/log/backup_mysql_db.log
 echo "${date} ${time} 开始备份" >> $backup_log
-
+mycnf="--defaults-extra-file=/data/service/mysql/my57.cnf"
+umysqldump="/data/service/mysql/bin/mysqldump"
+umysql="/data/service/mysql/bin/mysql"
 
 
 # 建立备份目录
@@ -19,19 +20,26 @@ fi
 # 删除以前备份
 
 # 备份 
-/data/service/mysql/bin/mysql --defaults-extra-file=/data/service/mysql/my57.cnf   -uroot  -A -N  -e  "show databases" | while read line
+${umysql} ${mycnf}  -uroot  -A -N  -e  "show databases" | while read line
 do
+    # 排除不需要备份的表
     db_names=$(echo $line | grep -v Database | grep -v information_schema | grep -v performance_schema | grep -v sys | grep -v mysql  )
     for db_name in  ${db_names}
     do
         echo ${db_name}
         # 备份数据
-        /data/service/mysql/bin/mysqldump  --defaults-extra-file=/data/service/mysql/my57.cnf  -uroot  --opt --single-transaction   --databases ${db_name}  > ${backup_dir}/${db_name}.sql
+        ${umysqldump}  ${mycnf}  -uroot  --opt --single-transaction   --databases ${db_name}  > ${backup_dir}/${db_name}.sql
         # 备份表结构
-        /data/service/mysql/bin/mysqldump  --defaults-extra-file=/data/service/mysql/my57.cnf  -uroot  --opt --single-transaction   --databases -d ${db_name}  > ${backup_dir}/${db_name}_struc.sql
-        # 备份TXT
-        #mkdir -p ${backup_dir}/${db_name}
-        #/data/service/mysql/bin/mysqldump  --defaults-extra-file=/data/service/mysql/my57.cnf  -uroot  --opt --single-transaction   -T${backup_dir}/${db_name} ${db_name}
+        ${umysqldump}  ${mycnf}  -uroot  --opt --single-transaction   --databases -d ${db_name}  > ${backup_dir}/${db_name}_struc.sql
+        # 备份单表
+        mkdir -p ${backup_dir}/${db_name}
+        ${umysql} ${mycnf}   -uroot  -A -N  -e  "use ${db_name}; show tables" | while read line
+        do
+            table_name=$(echo $line )
+            #echo ${table_name}
+            ${umysqldump}  ${mycnf}  -uroot  --opt --single-transaction    ${db_name} ${table_name}  > ${backup_dir}/${db_name}/${table_name}.sql
+        done
+
     done
 done
 echo "${date} ${time} 备份结束" >> $backup_log
