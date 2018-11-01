4876beca2b5b5b5fa14974bff3dfa24cb89b2c9b
ehml79 <ehml79@foxmail.com>
2018-11-01 17:41:20 +0800

    update

--- install/rsync/rsync_recv.sh
+++ install/rsync/rsync_recv.sh
@@ -7,4 +7,3 @@ echo "rsync_password" >  /etc/rsyncd/rsyncd.pass
 chmod 600  /etc/rsyncd/rsyncd.pass
 
 
-echo "rsync -vzrtopg --delete --progress --password-file=/etc/rsyncd/rsyncd.pass  --exclude "*access*" --exclude "debug" backup@192.168.172.129::backup  /data/backup/192.168.172.129"  > /data/sh/backup/rsync.sh
