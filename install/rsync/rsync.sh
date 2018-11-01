6195e285c277063a801c39806f98c84273f4ea65
ehml79 <ehml79@foxmail.com>
2018-11-01 16:57:13 +0800

    add rsync.sh

---
+++ install/rsync/rsync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+rsync_log=/data/backup/log/rsync.log
+
+
+
+start_ctime=$(date +%s)
+
+echo '-------------------------------------------------------------' >> ${rsync_log} 
+
+echo "$(date '+%F %T %s') ${0} ${@} 开始同步" >> ${rsync_log}
+echo >> ${rsync_log} 
+
+rsync -vzrtopg --delete --progress --password-file=/etc/rsyncd/rsyncd.pass  --exclude "*access*" --exclude "debug" backup@192.168.172.129::backup  /data/backup/192.168.172.129  >> ${rsync_log} 2>&1
+end_ctime=$(date +%s)
+
+echo >> ${rsync_log} 
+echo "$(date '+%F %T %s') ${0} ${@} 同步结束 脚本用时:$((${end_ctime}-${start_ctime}))s " >> $rsync_log
