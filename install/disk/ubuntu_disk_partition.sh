35af499f8317cde18d8f9dd937666883ea39a59e
ehml79 <ehml79@foxmail.com>
2018-11-07 14:52:23 +0800

    update disk

---
+++ install/disk/ubuntu_disk_partition.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 磁盘分区
+function ubuntu_disk_partition(){
+	echo -e "${RED}开始分区...${END}"
+
+	echo "n
+	p
+
+
+
+	w
+	" | fdisk /dev/vdb
+	mkfs.ext4 /dev/vdb1
+	cp /etc/fstab /etc/fstab.bak
+	mkdir /data
+	echo '/dev/vdb1 /data ext4 barrier=0 0 0' >> /etc/fstab
+	mount /dev/vdb1 /data/
+    rmdir /data/lost+found/
+}
+
+ubuntu_disk_partition
