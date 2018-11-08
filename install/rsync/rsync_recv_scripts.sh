110ff903dbfc518f406a4afedb8008409c4bb698
ehml79 <ehml79@foxmail.com>
2018-11-08 23:12:46 +0800

    add rsync

--- install/rsync/rsync_recv_scripts.sh
+++ install/rsync/rsync_recv_scripts.sh
@@ -3,7 +3,7 @@
 
 rsync_log=/data/backup/log/rsync.log
 speed='--bwlimit=5000'
-ip_list=
+ip_list=()
 
 
 
@@ -15,7 +15,7 @@ echo "$(date '+%F %T %s') ${0} ${@} 开始同步" >> ${rsync_log}
 echo >> ${rsync_log} 
 
 
-for remote_ip in ${ip_list}
+for remote_ip in ${ip_list[@]}
 do
     rsync -vzrtopg --delete --progress ${speed} --password-file=/etc/rsyncd/rsyncd.pass  --exclude "*access*" --exclude "debug" backup@${remote_ip}::backup  /data/backup/rsync/ygd/${remote_ip}  >> ${rsync_log} 2>&1
 done
