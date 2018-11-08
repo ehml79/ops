f53bc1d3a08de99ac516f3d029f364b3bbada728
ehml79 <ehml79@foxmail.com>
2018-11-08 15:49:16 +0800

    update recv

--- install/rsync/rsync_recv_scripts.sh
+++ install/rsync/rsync_recv_scripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-rsync_user=backup
-
 rsync_log=/data/backup/log/rsync.log
+speed='--bwlimit=5000'
+ip_list=
 
 
 
@@ -14,8 +14,14 @@ echo '-------------------------------------------------------------' >> ${rsync_
 echo "$(date '+%F %T %s') ${0} ${@} 开始同步" >> ${rsync_log}
 echo >> ${rsync_log} 
 
-rsync -vzrtopg --delete --progress --password-file=/etc/rsyncd/rsyncd.pass  --exclude "*access*" --exclude "debug" ${rsync_user}@192.168.172.129::${rsync_user}  /data/backup/192.168.172.129  >> ${rsync_log} 2>&1
-end_ctime=$(date +%s)
 
+for remote_ip in ${ip_list}
+do
+    rsync -vzrtopg --delete --progress ${speed} --password-file=/etc/rsyncd/rsyncd.pass  --exclude "*access*" --exclude "debug" backup@${remote_ip}::backup  /data/backup/rsync/ygd/${remote_ip}  >> ${rsync_log} 2>&1
+done
+
+
+
+end_ctime=$(date +%s)
 echo >> ${rsync_log} 
 echo "$(date '+%F %T %s') ${0} ${@} 同步结束 脚本用时:$((${end_ctime}-${start_ctime}))s " >> $rsync_log
