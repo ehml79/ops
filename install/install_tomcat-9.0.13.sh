31f2e733b558c0ac9254f45b9ffd8ecd9e4d8188
ehml79 <ehml79@foxmail.com>
2018-11-09 19:47:29 +0800

    add install tomcat

---
+++ install/install_tomcat-9.0.13.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+mkdir -p  /data/service/src
+
+wget http://mirrors.hust.edu.cn/apache/tomcat/tomcat-9/v9.0.13/bin/apache-tomcat-9.0.13.tar.gz -P  /data/service/src
+
+tar xf /data/service/src/apache-tomcat-9.0.13.tar.gz -C /data/service/
+mv /data/service/apache-tomcat-9.0.13/ /data/service/tomcat
+
+
+echo "bash  /data/service/tomcat/bin/startup.sh" > /root/tomcat_start.sh
+echo " bash /data/service/tomcat/bin/shutdown.sh" > /root/tomcat_stop.sh
