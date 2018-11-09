6062d7571937d65385393ae532c36e29bac30b38
ehml79 <ehml79@foxmail.com>
2018-11-09 19:02:42 +0800

    update install java

--- install/java/install_java-8.sh
+++ install/java/install_java-8.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
+mkdir -p /data/service/
+
 tar xf jdk-8u191-linux-x64.tar.gz -C /data/service/
 mv /data/service/jdk-8u191-linux-x64 /data/service/jdk
 
 
-bash_file=
+bash_file="/etc/profile"
 
 echo "export JAVA_HOME=/data/service/jdk/"  >>  ${bash_file}
 echo "export JRE_HOME=\${JAVA_HOME}/jre"  >> ${bash_file}
