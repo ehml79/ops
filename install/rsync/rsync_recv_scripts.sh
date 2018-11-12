fb0166a5a90a6984c6ecfab02fa525ab162e5724
ehml79 <ehml79@foxmail.com>
2018-11-12 09:50:43 +0800

    update rsync scripts

--- install/rsync/rsync_recv_scripts.sh
+++ install/rsync/rsync_recv_scripts.sh
@@ -3,7 +3,11 @@
 
 rsync_log=/data/backup/log/rsync.log
 speed='--bwlimit=5000'
+#delete='--delete'
 ip_list=()
+
+relRsync="rsync -vzrtopg ${delete} ${speed}  --progress --password-file=/etc/rsyncd/rsyncd.pass  --exclude '*access*' --exclude 'debug'"
+
 
 
 
@@ -17,7 +21,7 @@ echo >> ${rsync_log}
 
 for remote_ip in ${ip_list[@]}
 do
-    rsync -vzrtopg --delete --progress ${speed} --password-file=/etc/rsyncd/rsyncd.pass  --exclude "*access*" --exclude "debug" backup@${remote_ip}::backup  /data/backup/rsync/ygd/${remote_ip}  >> ${rsync_log} 2>&1
+    ${relRsync} backup@${remote_ip}::backup  /data/backup/rsync/ygd/${remote_ip}  >> ${rsync_log} 2>&1
 done
 
 
