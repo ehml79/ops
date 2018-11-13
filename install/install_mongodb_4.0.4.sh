7481e45053a72476c1ac95a7d7ffa81bf1676684
ehml79 <ehml79@foxmail.com>
2018-11-13 11:11:15 +0800

    add install mongodb

---
+++ install/install_mongodb_4.0.4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+function install_mongodb(){
+    wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-4.0.4.tgz -P /data/service/src/ 
+    cd /data/service/src
+    tar xf mongodb-linux-x86_64-4.0.4.tgz
+    cd mongodb-linux-x86_64-4.0.4/
+    mv mongodb-linux-x86_64-4.0.4 /data/service/mongodb
+    mkdir -p /data/service/mongodb/data
+    touch /data/service/mongodb/mongodb.log
+    
+    /data/service/mongodb/bin/mongod --dbpath=/data/service/mongodb/data/  --logpath=/data/service/mongodb/mongodb.log -logappend -port=27017 --fork
+    
+    echo "PATH=$PATH:/data/service/mongodb/bin/" >>/etc/profile
+    export PATH=$PATH:/data/service/mongodb/bin/
+}
+
+
+install_mongodb
