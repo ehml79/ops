136d5dfc723e04346248c0b5be03056dfbc94bb7
ehml79 <ehml79@foxmail.com>
2018-11-14 16:36:56 +0800

    update trigger

--- install/jenkins/trigger.sh
+++ install/jenkins/trigger.sh
@@ -6,6 +6,8 @@ passwd=
 ip=
 project=
 token=
+verbose="-v"
+method=post
 
 
-curl -X post -v -u user:passwd  http://${ip}:8080/jenkins/job/${project}/build?token=${token} 
+curl -X ${method} ${verbose} -u ${user}:${passwd}  http://${ip}:8080/jenkins/job/${project}/build?token=${token} 
