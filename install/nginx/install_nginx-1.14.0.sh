b4a7157a8cd4d26b56570e8dede4f81885d43fc8
ehml79 <ehml79@foxmail.com>
2018-11-15 17:04:24 +0800

    update install nginx

--- install/nginx/install_nginx-1.14.0.sh
+++ install/nginx/install_nginx-1.14.0.sh
@@ -17,9 +17,15 @@ install_nginx(){
     	exit 1
     fi
     
+    # install openssl
+
+    wget https://www.openssl.org/source/openssl-1.1.1.tar.gz -P /data/service/src
+    cd /data/service/src
+    tar xf  openssl-1.1.1.tar.gz
     
-    groupadd ${web_user}
-    useradd -M -s /sbin/nologin -g ${web_user}  ${web_user}
+
+    groupadd ${run_user}
+    useradd -M -s /sbin/nologin -g ${run_user}  ${run_user}
     mkdir -p /data/service/src
     wget http://nginx.org/download/nginx-1.14.0.tar.gz  -P /data/service/src
     cd /data/service/src ; tar xf  nginx-1.14.0.tar.gz
@@ -33,7 +39,6 @@ install_nginx(){
     --with-http_ssl_module \
     --with-http_stub_status_module \
     --with-http_v2_module \
-    --with-http_spdy_module \
     --with-http_gzip_static_module \
     --with-http_sub_module \
     --with-http_realip_module \
@@ -41,30 +46,22 @@ install_nginx(){
     --with-http_mp4_module \
     --with-stream \
     --with-stream_ssl_module \
-    --with-openssl=../openssl-${openssl_ver} \
-    --with-pcre-jit \
-    --with-ipv6 \
-    --with-ld-opt='-ljemalloc' \
-    ${nginx_modules_options}
-    ${Nginx_Modules_Options}
-    ${Nginx_Modules_Options}
-    ${Nginx_Module_Lua} 
-    ${Nginx_Module_Lua} 
-    ${NginxMAOpt} 
-    ${NginxMAOpt} 
+    --with-openssl=../openssl-1.1.1 \
+    --with-pcre-jit 
 
 
     
     make  && make install
 
     mkdir -p /data/service/nginx/conf/vhost/
-    sed -i "/worker_processes/i\user  ${web_user};"  /data/service/nginx/conf/nginx.conf
+    sed -i "/worker_processes/i\user  ${run_user};"  /data/service/nginx/conf/nginx.conf
     sed -i '/#tcp_nopush/a\    include vhost/*.conf;'  /data/service/nginx/conf/nginx.conf
     
 
 
     echo 'export PATH=$PATH:/data/service/nginx/sbin' >> /etc/profile
     source /etc/profile
+    . /etc/profile
     export PATH=$PATH:/data/service/nginx/sbin
 
 
