e869c40c51eddb8b1b6fb5ac4a5fa5a0ba3b0372
ehml79 <ehml79@foxmail.com>
2018-11-15 16:55:50 +0800

    add install pmm

--- install/mysql/install_pmm-1.16.0.sh
+++ install/mysql/install_pmm-1.16.0.sh
@@ -5,5 +5,10 @@
 # wget https://www.percona.com/downloads/pmm/1.16.0/binary/tarball/pmm-client-1.16.0.tar.gz
 # Kibana 
 
-wget https://www.percona.com/downloads/pmm/1.16.0/source/tarball/pmm-client-1.16.0.tar.gz
+wget https://www.percona.com/downloads/pmm/1.16.0/binary/tarball/pmm-client-1.16.0.tar.gz -P /data/service/src
+
+cd /data/service/src
+tar xf pmm-client-1.16.0.tar.gz
+cd pmm-client-1.16.0/
+
 
