3d097701afc0903f1a6c33bfc7edb20ec2453f94
ehml79 <ehml79@foxmail.com>
2018-11-16 15:58:18 +0800

    update install phpmyadmin

--- install/php/install_phpmyadmin-4.8.3.sh
+++ install/php/install_phpmyadmin-4.8.3.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 
-wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-all-languages.tar.gz
+mkdir -p /data/{service,web}
 
-tar xf phpMyAdmin-4.8.3-all-languages.tar.gz
+wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-all-languages.tar.gz -P /data/service/src
+
+tar xf /data/service/src/phpMyAdmin-4.8.3-all-languages.tar.gz -C /data/web
+
+
+cd /data/web/ &&  mv phpMyAdmin-4.8.3-all-languages/ phpMyAdmin
 
 
 
