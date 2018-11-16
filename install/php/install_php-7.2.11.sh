f7f4191b0d4318e3587f97bb81397ba0b23860bf
ehml79 <ehml79@foxmail.com>
2018-11-16 15:01:21 +0800

    update install php

--- install/php/install_php-7.2.11.sh
+++ install/php/install_php-7.2.11.sh
@@ -3,6 +3,26 @@
 web_user=www
 
 
+function install_openssl(){
+    # install openssl
+
+#    wget https://www.openssl.org/source/openssl-1.1.1.tar.gz -P /data/service/src
+    cd /data/service/src
+    tar xf  openssl-1.1.1.tar.gz
+    cd openssl-1.1.1/
+    ./config -fPIC --prefix=/data/service/openssl --openssldir=/data/service/openssl
+    make && make install 
+
+
+    ln -s /data/service/openssl/lib/libssl.so.1.1 /usr/lib/libssl.so.1.1
+    ln -s /data/service/openssl/lib/libcrypto.so.1.1 /usr/lib/libcrypto.so.1.1
+
+    echo 'export PATH=$PATH:/data/service/openssl/bin/' >>/etc/profile
+
+
+}
+
+
 function install_php(){
 
     groupadd ${web_user}
@@ -10,10 +30,13 @@ function install_php(){
     # 判断系统
     if [ -f /etc/os-release ];then
         echo 'ubuntu'
-        sudo apt-get -y install git libpcre3 libpcre3-dev  zlib1g-dev  \
-        build-essential libxml2-dev openssl libssl-dev make curl \
-        libcurl4-gnutls-dev libjpeg-dev libpng-dev  libmcrypt-dev \
-        libcurl4-openssl-dev pkg-config libxml2-dev openssl  libfreetype6-dev  libmcrypt-dev 
+        sudo apt -y install git libpcre3 libpcre3-dev  \
+        zlib1g-dev build-essential libxml2-dev openssl \
+        libssl-dev make curl libcurl4-openssl-dev \
+        libjpeg-dev libpng-dev  libmcrypt-dev libcurl4-gnutls-dev \
+        libxslt-dev pkg-config libxml2-dev openssl  \
+        libfreetype6-dev  libmcrypt-dev  libsodium-dev \
+        argon2 libargon2-0 libargon2-0-dev
     elif [ -f /etc/redhat-release ];then
         echo 'centOS'
         yum install -y git gcc gcc-c++  make zlib zlib-devel pcre pcre-devel  \
@@ -30,27 +53,62 @@ function install_php(){
 
 
     # http://cn2.php.net/distributions/php-7.2.11.tar.xz
-    wget http://cn.php.net/distributions/php-7.2.11.tar.gz  -P /data/service/src/
+#    wget http://cn.php.net/distributions/php-7.2.11.tar.gz  -P /data/service/src/
     
     cd /data/service/src/
     tar xf php-7.2.11.tar.gz
     cd php-7.2.11
 
-    ./configure --prefix=/data/service/php \
+    ./configure \
+    --prefix=/data/service/php \
+    --with-fpm-group=www \
+    --with-fpm-user=www \
     --with-config-file-path=/data/service/php/etc \
-    --with-config-file-scan-dir=/data/service/php/conf.d \
-    --enable-fpm --with-fpm-user=${web_user} \
-    --with-fpm-group=${web_user} --enable-mysqlnd \
-    --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd \
-    --with-iconv-dir --with-freetype-dir=/usr/local/freetype \
-    --with-jpeg-dir --with-png-dir --with-zlib --with-libxml-dir=/usr \
-    --enable-xml --disable-rpath --enable-bcmath --enable-shmop \
-    --enable-sysvsem --enable-inline-optimization --with-curl \
-    --enable-mbregex --enable-mbstring --enable-intl \
-    --enable-pcntl --enable-ftp --with-gd --with-openssl \
-    --with-mhash --enable-pcntl --enable-sockets --with-xmlrpc \
-    --enable-zip --enable-soap --with-gettext  \
-    --enable-opcache --with-xsl
+    --with-config-file-scan-dir=/data/service/php/etc \
+    --with-curl=shared,/usr \
+    --with-freetype-dir=/usr/local/freetype \
+    --with-sodium=/usr/local \
+    --with-libxml-dir=/usr \
+    --with-iconv-dir=/usr/local \
+    --with-libxml-dir=/usr \
+    --with-openssl=/data/service/openssl \
+    --enable-fpm \
+    --enable-bcmath \
+    --enable-mysqlnd \
+    --enable-ftp \
+    --with-pdo-mysql=mysqlnd \
+    --with-gd \
+    --enable-xml \
+    --enable-zip \
+    --enable-mbstring \
+    --enable-shmop \
+    --with-curl \
+    --with-xsl \
+    --with-mhash \
+    --with-iconv-dir \
+    --with-gettext \
+    --enable-mbregex \
+    --with-zlib \
+    --enable-pcntl \
+    --enable-sockets \
+    --enable-soap \
+    --enable-inline-optimization \
+    --enable-intl \
+    --enable-opcache \
+    --with-xmlrpc \
+    --with-mysqli=mysqlnd \
+    --with-jpeg-dir \
+    --enable-sysvsem \
+    --disable-rpath \
+    --with-png-dir \
+    --disable-rpath \
+    --disable-debug \
+    --disable-fileinfo \
+    --enable-exif \
+    #--with-password-argon2  \
+    --with-png-dir 
+
+    exit
 
     make && make install
 
@@ -65,15 +123,15 @@ function install_php(){
 
 
 
-    # mcrypt
-    wget wget http://pecl.php.net/get/mcrypt-1.0.1.tgz  -P  /data/service/src/
-    cd /data/service/src/
-    tar xf mcrypt-1.0.1.tgz 
-    cd mcrypt-1.0.1
-    /data/service/php/bin/phpize
-    ./configure
-    make && sudo make install
-    echo "extension=mcrypt.so" >> /data/service/php/etc/php.ini
+   # mcrypt
+   # wget http://pecl.php.net/get/mcrypt-1.0.1.tgz  -P  /data/service/src/
+   # cd /data/service/src/
+   # tar xf mcrypt-1.0.1.tgz 
+   # cd mcrypt-1.0.1
+   # /data/service/php/bin/phpize
+   # ./configure
+   # make && sudo make install
+   # echo "extension=mcrypt.so" >> /data/service/php/etc/php.ini
     echo "security.limit_extensions = .php .php3 .php4 .php5 .do .html" >> /data/service/php/etc/php.ini
 
     # 启动 php
@@ -82,5 +140,7 @@ function install_php(){
 
 }
 
+
+install_openssl
 
 install_php
