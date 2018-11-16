f2efb5653a421905afcd0a87e326d988ef059a1a
ehml79 <ehml79@foxmail.com>
2018-11-16 11:29:34 +0800

    add install phpmyadmin

--- install/php/install_phpmyadmin-4.8.3.sh
+++ install/php/install_phpmyadmin-4.8.3.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 
-wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-all-languages.zip 
+wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-all-languages.tar.gz
+
+tar xf phpMyAdmin-4.8.3-all-languages.tar.gz
+
+
+
