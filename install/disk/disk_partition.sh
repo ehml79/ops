1aa1068d3ec2291bf0dfd5754867f0e1449cfd6c
ehml79 <ehml79@foxmail.com>
2018-11-20 11:06:37 +0800

    update disk partitions

--- install/disk/disk_partition.sh
+++ install/disk/disk_partition.sh
@@ -33,7 +33,7 @@ function centos_disk_partition(){
     mkfs.ext4 /dev/vdb1
     cp /etc/fstab /etc/fstab.bak
     mkdir /data
-    echo '/dev/vdb1 /data ext3 defaults 0 0' >> /etc/fstab
+    echo '/dev/vdb1 /data ext4 defaults 0 0' >> /etc/fstab
     mount /dev/vdb1 /data/
     rmdir /data/lost+found/
 }
