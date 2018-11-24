84e5305db1e79d31bc138c0030f693d022b50be7
ehml79 <ehml79@foxmail.com>
2018-11-24 14:32:37 +0800

    update install prometheus

--- install/grafana/install_prometheus.sh
+++ install/grafana/install_prometheus.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
+your_domain=domain
+
+
 function install_prometheus(){
     mkdir -p /data/service/src/
     wget https://github.com/prometheus/prometheus/releases/download/v2.5.0/prometheus-2.5.0.linux-amd64.tar.gz -P /data/service/src/
@@ -15,6 +18,23 @@ function install_prometheus(){
     /data/service/prometheus/prometheus --config.file="/data/service/prometheus/prometheus.yml"  >> /data/service/prometheus/log/prometheus.log 2>&1 & 
     
     # 根据自身服务器，修改 prometheus.yml 
+
+    # 生成nginx 访问地址
+    cat > /data/service/nginx/conf/vhost/prometheus.conf <<EOF
+#
+server {
+        listen       80;
+        server_name  ${your_domain};
+
+        charset utf-8;
+
+        location / {
+            default_type text/html;
+            proxy_pass http://127.0.0.1:9090;
+        }
+
+}
+EOF
 
 }
 
