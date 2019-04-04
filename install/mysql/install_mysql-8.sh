a8e9018abf0b2f7e4ee1e86ebb8c341acfcaa4db
ehml79 <ehml79@foxmail.com>
2019-04-04 14:09:01 +0800

    rename and modified

--- install/mysql/install_mysql-8.sh
+++ install/mysql/install_mysql-8.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+mysql_version="mysql-8.0.15"
+
 sudo apt -y install make cmake gcc g++ bison libncurses5-dev build-essential libssl-dev  libaio1
 
 groupadd mysql
@@ -8,15 +10,15 @@ useradd -r -g mysql -s /bin/false mysql
 
 mkdir -p /data/service/src/
 
-#wget https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-8.0.13.tar.gz -P /data/service/src/
+#wget https://cdn.mysql.com//Downloads/MySQL-8.0/${mysql_version}.tar.gz -P /data/service/src/
 #wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz  -P /data/service/src/
 
-wget https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.13-linux-glibc2.12-x86_64.tar.xz -P /data/service/src/
+wget https://dev.mysql.com/get/Downloads/MySQL-8.0/${mysql_version}-linux-glibc2.12-x86_64.tar.xz -P /data/service/src/
 
 
 cd /data/service/src
-tar xf mysql-8.0.13-linux-glibc2.12-x86_64.tar.xz 
-mv /data/service/src/mysql-8.0.13-linux-glibc2.12-x86_64 /data/service/mysql
+tar xf ${mysql_version}-linux-glibc2.12-x86_64.tar.xz 
+mv /data/service/src/${mysql_version}-linux-glibc2.12-x86_64 /data/service/mysql
 cd /data/service/mysql
 bin/mysqld --initialize-insecure --user=mysql  --basedir=/data/service/mysql --datadir=/data/service/mysql/data/
 
