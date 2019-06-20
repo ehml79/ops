d24142ee80ac67916b0b8e9da3841a948be56800
ehml79 <ehml79@foxmail.com>
2019-06-20 11:41:45 +0800

    add install tomcat

--- install/tomcat/install_tomcat-9.0.13.sh
+++ install/tomcat/install_tomcat-9.0.13.sh
@@ -3,10 +3,10 @@
 
 mkdir -p  /data/service/src
 
-wget -O /data/service/src/apache-tomcat-9.0.13.tar.gz http://mirrors.hust.edu.cn/apache/tomcat/tomcat-9/v9.0.13/bin/apache-tomcat-9.0.13.tar.gz 
+wget -O /data/service/src/apache-tomcat-9.0.21.tar.gz http://mirrors.hust.edu.cn/apache/tomcat/tomcat-9/v9.0.21/bin/apache-tomcat-9.0.21.tar.gz 
 
-tar xf /data/service/src/apache-tomcat-9.0.13.tar.gz -C /data/service/
-mv /data/service/apache-tomcat-9.0.13/ /data/service/tomcat
+tar xf /data/service/src/apache-tomcat-9.0.21.tar.gz -C /data/service/
+mv /data/service/apache-tomcat-9.0.21/ /data/service/tomcat
 
 
 echo "bash  /data/service/tomcat/bin/startup.sh" > /root/tomcat_start.sh
