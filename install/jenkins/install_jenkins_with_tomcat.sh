ee428dd1b3714f0b4ec823f071163ab020eba5ff
ehml79 <ehml79@foxmail.com>
2019-06-20 13:00:33 +0800

    update install jenkins

--- install/jenkins/install_jenkins_with_tomcat.sh
+++ install/jenkins/install_jenkins_with_tomcat.sh
@@ -3,7 +3,7 @@
 # install java
 # install tomcat
 
-wget -O /data/server/src/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war 
-mv  /data/server/src/jenkins.war  /data/service/tomcat/webapps
+wget -O /data/service/src/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war 
+mv  /data/service/src/jenkins.war  /data/service/tomcat/webapps
 # 启动tomcat
 # 访问url
