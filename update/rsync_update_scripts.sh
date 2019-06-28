41e9c535b4b3c1c23d071e2c349990397982ffbb
ehml79 <ehml79@foxmail.com>
2019-06-28 09:47:10 +0800

    update rsync update

--- update/rsync_update_scripts.sh
+++ update/rsync_update_scripts.sh
@@ -2,7 +2,7 @@
 
 # rsync 配合 svn更新
 
-# crontab 
+# crontab
 # * * * * *            /bin/bash  /data/sh/update/rsync_update_scripts.sh
 # * * * * * sleep  5 ; /bin/bash  /data/sh/update/rsync_update_scripts.sh
 # * * * * * sleep 10 ; /bin/bash  /data/sh/update/rsync_update_scripts.sh
@@ -18,33 +18,30 @@
 
 
 chown -R www.www /data/svn && chmod -R 775 /data/svn
-
 rsync_log=/data/log/rsync_update_scripts.log
-speed='--bwlimit=5000'
+#speed='--bwlimit=5000'
 progress='--progress'
 delete='--delete'
 
-relRsync="rsync -vzrtopg  ${speed} ${progress} --password-file=/etc/rsyncd/rsyncd.pass "
-
-start_ctime=$(date +%s)
-
-echo '-----' >> ${rsync_log} 
-echo "$(date '+%F %T %s') ${0} ${@} 开始更新" >> ${rsync_log}
-echo >> ${rsync_log} 
+relRsync="rsync -vzrtopg  ${speed} ${progress} --password-file=/etc/rsyncd/rsyncd.pass"
 
 # 更新svn
-/usr/bin/svn up /data/svn/repo  >> ${rsync_log} 
+line_number=$(/usr/bin/svn up /data/svn/repo | wc -l)
 
-flag=push
+# 判断svn有没有新提交
+if [ ${line_number}  -gt 2 ];then
 
-# 接收文件
-if [ ${flag} == 'recv' ];then
-    ${relRsync} rsync@192.168.0.1::www  /data/svn/repo/www/  >> ${rsync_log} 2>&1
-# 推送文件
-elif [ ${flag} == 'push' ];then
-    ${relRsync} /data/svn/repo/www/ rsync@192.168.0.1::www  >> ${rsync_log} 2>&1
+    # 分割空格
+    echo  | tee -a  ${rsync_log} 
+    echo  | tee -a  ${rsync_log} 
+
+    start_ctime=$(date +%s)
+    echo "$(date '+%F %T %s') ${0} ${@} 开始更新" | tee -a  ${rsync_log}
+
+    ${relRsync} --exclude="*.apk" --exclude="*.log" /data/svn/repo/www/ rsync@192.168.0.7::www  | tee -a  ${rsync_log} 
+
+    end_ctime=$(date +%s)
+    echo "$(date '+%F %T %s') ${0} ${@} 结束更新 脚本用时:$((${end_ctime}-${start_ctime}))s " | tee -a  ${rsync_log}
+else
+	echo "No file to update ..."
 fi
-
-end_ctime=$(date +%s)
-echo >> ${rsync_log} 
-echo "$(date '+%F %T %s') ${0} ${@} 更新结束 脚本用时:$((${end_ctime}-${start_ctime}))s " >> $rsync_log
