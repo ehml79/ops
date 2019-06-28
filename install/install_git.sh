9d19d7337e8213fb7483978ed31c17cb0d22d791
ehml79 <ehml79@foxmail.com>
2019-06-28 12:00:39 +0800

    add install git

---
+++ install/install_git.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+your_authorized_keys=
+
+
+sudo apt-get install git
+
+sudo adduser git
+
+mkdir -p /data/service/git
+
+cd /data/service/git
+
+sudo git init --bare repo.git
+
+sudo chown -R git:git repo.git
+
+
+# 更改 git shell
+sed -i '/^git/s@/bin/bash@/usr/bin/git-shell@' /etc/passwd
+
+
+mkdir -p /home/git/.ssh
+
+
+echo "${your_authorized_keys}"  >>  /home/git/.ssh/authorized_keys
