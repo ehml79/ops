8e3f39e98d26b3724a3a7e3953af889b504251a7
ehml79 <ehml79@foxmail.com>
2019-06-30 17:55:40 +0800

    update

--- install/tomcat/install_tomcat-9.0.21.sh
+++ install/tomcat/install_tomcat-9.0.21.sh
@@ -8,6 +8,24 @@ wget -O /data/service/src/apache-tomcat-9.0.21.tar.gz http://mirrors.tuna.tsingh
 tar xf /data/service/src/apache-tomcat-9.0.21.tar.gz -C /data/service/
 mv /data/service/apache-tomcat-9.0.21/ /data/service/tomcat
 
+cat >  /root/tomcat_start.sh <<EOF
+#!/bin/bash 
+/bin/bash  /data/service/tomcat/bin/startup.sh
+EOF 
 
-echo "bash  /data/service/tomcat/bin/startup.sh" > /root/tomcat_start.sh
-echo " bash /data/service/tomcat/bin/shutdown.sh" > /root/tomcat_stop.sh
+chmod +x /root/tomcat_start.sh
+
+cat > /root/tomcat_stop.sh <<EOF
+#!/bin/bash 
+/bin/bash /data/service/tomcat/bin/shutdown.sh
+EOF
+
+chmod +x /root/tomcat_stop.sh
+
+cat > <<EOF
+#!/bin/bash 
+/bin/bash /data/service/tomcat/bin/shutdown.sh && /bin/bash  /data/service/tomcat/bin/startup.sh
+EOF
+
+chmod +x /root/tomcat_restart.sh
+
