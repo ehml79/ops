0ee58f960e9267cd7b30de475de75acfbdd85ecd
ehml79 <ehml79@foxmail.com>
2019-07-01 10:50:14 +0800

    update

---
+++ update/rsync_update_scripts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# rsync 配合 svn更新
+chown -R www.www /data/svn && chmod -R 775 /data/svn
+
+rsync_log=/data/log/rsync_update_scripts.log
+#speed='--bwlimit=5000'
+progress='--progress'
+delete='--delete'
+
+relRsync="rsync -vzrtopg  ${speed} ${progress} --password-file=/etc/rsyncd/rsyncd.pass"
+
+# 更新svn
+line_number=$(/usr/bin/svn up /data/svn/repo | wc -l)
+
+# 判断svn有没有新提交
+if [ ${line_number}  -gt 2 ];then
+
+    # 分割空格
+    echo  >> ${rsync_log} 
+    echo  >> ${rsync_log} 
+
+    start_ctime=$(date +%s)
+    echo "$(date '+%F %T %s') ${0} ${@} 开始更新" >> ${rsync_log}
+
+    # sdk 
+    ${relRsync} --exclude="*.apk" --exclude="*.log" /data/svn/repo/sdk/ rsync@192.168.0.3::sdk  | tee -a  ${rsync_log}
+
+    end_ctime=$(date +%s)
+    echo "$(date '+%F %T %s') ${0} ${@} 结束更新 脚本用时:$((${end_ctime}-${start_ctime}))s " >> $rsync_log
+else
+	echo "No file to update ..."
+fi
