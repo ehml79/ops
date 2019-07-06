9f89adb8a097af58fb8c7c96f7fa4974d10e270b
ehml79 <ehml79@foxmail.com>
2019-07-06 10:29:57 +0800

    update

--- install/svn/svn_dump.sh
+++ install/svn/svn_dump.sh
@@ -3,6 +3,3 @@
 
 # 导出
 svnadmin dump  /data/svn/repo > repo.dump
-
-# 导入
-svnadmin load  /data/svn/newrepo < repo.dump
---
+++ install/svn/svn_load.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 导入
+svnadmin load  /data/svn/newrepo < repo.dump
