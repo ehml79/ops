d534312a812f0cea8053e7269c9f04334f2c65f5
ehml79 <ehml79@foxmail.com>
2019-08-02 12:02:50 +0800

    add config sshd

--- install/mysql/install_mysql-8.sh
+++ install/mysql/install_mysql-8.sh
@@ -33,4 +33,25 @@ mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_pass
 export PATH=$PATH:/data/service/mysql/bin
 echo 'export PATH=$PATH:/data/service/mysql/bin' >> /etc/profile
 
+function config_sshd(){
+
+    # navicat SSH 连接时提示
+    # does not support diffie-hellman-group1-sha1
+    # for keyexchange 或 The negotiation of encryption
+    # algorithm is failed的解决方法
+
+cat >> /etc/ssh/sshd_config <<EOF
+KexAlgorithms diffie-hellman-group1-sha1,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1
+Ciphers 3des-cbc,blowfish-cbc,aes128-cbc,aes128-ctr,aes256-ctr
+EOF
+
+/usr/bin/ssh-keygen -A
+
+/usr/sbin/service ssh restart
+
+
+}
+
+config_sshd
+
 
