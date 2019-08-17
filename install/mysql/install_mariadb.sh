dae916082ce90ee6bdff00d4f107ab4e6681d0cf
ehml79 <ehml79@foxmail.com>
2019-08-17 11:11:27 +0800

    update config ssh

--- install/mysql/install_mariadb.sh
+++ install/mysql/install_mariadb.sh
@@ -154,30 +154,6 @@ EOF
 
 
 
-function config_sshd(){
-
-    # navicat SSH 连接时提示
-    # does not support diffie-hellman-group1-sha1
-    # for keyexchange 或 The negotiation of encryption
-    # algorithm is failed的解决方法
-
-cat >> /etc/ssh/sshd_config <<EOF
-KexAlgorithms diffie-hellman-group1-sha1,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1
-Ciphers 3des-cbc,blowfish-cbc,aes128-cbc,aes128-ctr,aes256-ctr
-EOF
-
-/usr/bin/ssh-keygen -A
-
-/usr/sbin/service ssh restart
-
-
-}
-
-
-
-
-
 install_mariadb
 
-#config_sshd
 rm /root/$0
