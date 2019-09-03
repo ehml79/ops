ee6a617440548929efb3a6fe43fcb01ff545bf99
ehml79 <ehml79@foxmail.com>
2019-09-03 17:57:24 +0800

    update install protobuf

--- install/php/install_protobuf.sh
+++ install/php/install_protobuf.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /data/service/src/
 
-wget -O /data/service/src/protobuf-php-3.9.1.tar.gz  https://github.com/protocolbuffers/protobuf/releases/download/v3.9.1/protobuf-php-3.9.1.tar.gz
+wget -O /data/service/src/protobuf-all-3.9.1.tar.gz  https://github.com/protocolbuffers/protobuf/releases/download/v3.9.1/protobuf-all-3.9.1.tar.gz
 
 cd /data/service/src
 tar xf  protobuf-all-3.9.1.tar.gz
