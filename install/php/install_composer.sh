eedec640aeffe8a651dd44abd50776de38721c3e
ehml79 <ehml79@foxmail.com>
2019-09-03 14:21:46 +0800

    add install composer

---
+++ install/php/install_composer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# nginx php mysql
+
+apt -y install unzip  wget
+
+mkdir -p /data/www
+cd /data/www
+wget https://getcomposer.org/download/1.8.0/composer.phar -O  /usr/bin/composer
+
+chmod +x /usr/bin/composer
+# 更改国内镜像
+/usr/bin/composer config -g repo.packagist composer https://packagist.phpcomposer.com
+mkdir -p /root/.config/composer/
+cat >/root/.config/composer/config.json << EOF
+{
+    "config": {},
+    "repositories": {
+        "packagist": {
+            "type": "composer",
+            "url": "https://packagist.phpcomposer.com"
+        }
+    }
+}
+EOF
+
+cat >/root/.config/composer/composer.json <<EOF
+{
+    "name": "laravel/laravel",
+    "description": "The Laravel Framework.",
+    "keywords": ["framework", "laravel"],
+    "license": "MIT",
+    "type": "project",
+    "require": {
+        "php": ">=5.5.9",
+        "laravel/framework": "5.6.*"
+    },
+    "config": {
+        "preferred-install": "dist"
+    },
+    "repositories": {
+        "packagist": {
+            "type": "composer",
+            "url": "https://packagist.phpcomposer.com"
+        }
+    }
+}
+EOF
+
