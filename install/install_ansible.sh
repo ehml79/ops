891f7cb2c7c161148e64d3734306028f78d23347
ehml79 <ehml79@foxmail.com>
2019-09-05 22:51:35 +0800

    update install ansible

--- install/install_ansible.sh
+++ install/install_ansible.sh
@@ -4,9 +4,9 @@
 
 #  install
 sudo apt update
-sudo apt install software-properties-common
+sudo apt -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt -y install ansible
 #sudo apt -y install  sshpass  python-minimal ansible
 
 # config
