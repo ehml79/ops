b7a53a1ada6203500b340dd27dda71d50a5ed220
ehml79 <ehml79@foxmail.com>
2019-09-05 22:29:17 +0800

    update install ansible

--- install/install_ansible.sh
+++ install/install_ansible.sh
@@ -3,7 +3,10 @@
 # for ubuntu
 
 #  install
-sudo apt -y install  sshpass ansible
+sudo apt -y install  sshpass  python-minimal ansible
+
+# config
+sed -i 's@#host_key_checking@host_key_checking@' /etc/ansible/ansible.cfg
 
 # test
 ansible all -m ping
