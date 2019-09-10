708291bc2844e613f48dd1abf9c5eadab43f77d8
ehml79 <ehml79@foxmail.com>
2019-09-10 17:53:34 +0800

    add alias

--- install/python/install_virtualenv.sh
+++ install/python/install_virtualenv.sh
@@ -10,10 +10,12 @@ cd /data/venv/
 virtualenv -p /usr/bin/python3 --no-site-packages py3
 
 # 启动虚拟环境
-source /data/venv/py3/bin/activate
+# source /data/venv/py3/bin/activate
 
 # 退出虚拟环境
-deactivate　
+# deactivate　
 
 # 删除虚拟环境
-rm /data/venv/py3
+# rm /data/venv/py3
+
+echo "alias py3='source /data/venv/py3/bin/activate'" >> /root/.bashrc
