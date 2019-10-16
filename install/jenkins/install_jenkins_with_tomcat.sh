9ef182a4fd87578e6ef5f1644df7604c9cdb6805
ehml79 <ehml79@foxmail.com>
2019-10-16 17:19:25 +0800

    update install_jenkins_with_tomcat.sh

--- install/jenkins/install_jenkins_with_tomcat.sh
+++ install/jenkins/install_jenkins_with_tomcat.sh
@@ -7,3 +7,8 @@ wget -O /data/service/src/jenkins.war http://mirrors.jenkins.io/war-stable/lates
 mv  /data/service/src/jenkins.war  /data/service/tomcat/webapps
 # 启动tomcat
 # 访问url
+
+
+# 启动jenkins
+echo "nohup java -jar /data/service/jenkins.war --httpPort=8080 > /data/service/jenkins.log 2>&1 &" > /root/jenkins_start.sh
+
