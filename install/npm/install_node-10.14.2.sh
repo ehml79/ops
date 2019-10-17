8ff159c2a8a708f06657a7c1e04446370e3d28eb
ehml79 <ehml79@foxmail.com>
2019-10-17 15:05:08 +0800

    update

--- install/npm/install_node-10.14.2.sh
+++ install/npm/install_node-10.14.2.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
+node_version=node-v10.16.3
 
-function install_vue(){
+
+function install_node(){
 
     mkdir -p /data/service/src
     
-    #wget -O /data/service/src/node-v10.14.2.tar.gz  https://nodejs.org/dist/v10.14.2/node-v10.14.2.tar.gz 
     
-    wget -O /data/service/src/node-v10.14.1-linux-x64.tar.gz  https://npm.taobao.org/mirrors/node/v10.14.1/node-v10.14.1-linux-x64.tar.gz 
-    tar xf /data/service/src/node-v10.14.1-linux-x64.tar.gz -C /data/service/
+    wget -O /data/service/src/${node_version}-linux-x64.tar.gz  https://npm.taobao.org/mirrors/node/v10.14.1/${node_version}-linux-x64.tar.gz 
+    tar xf /data/service/src/${node_version}-linux-x64.tar.gz -C /data/service/
     
-    mv /data/service/node-v10.14.1-linux-x64/  /data/service/node
+    mv /data/service/${node_version}-linux-x64/  /data/service/node
     
     echo 'export PATH=$PATH:/data/service/node/bin/' >>/etc/profile
     
@@ -28,4 +29,4 @@ function install_vue(){
 }
 
 
-install_vue
+install_node
