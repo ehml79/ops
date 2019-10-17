8e4d3f862bba617721d7e7612de674da8115d5c9
ehml79 <ehml79@foxmail.com>
2019-10-17 11:47:01 +0800

    update

--- install/jenkins/install_jenkins_with_tomcat.sh
+++ install/jenkins/install_jenkins_with_tomcat.sh
@@ -9,7 +9,8 @@ apt -y  install openjdk-8-jdk-headless openjdk-8-jre-headless
 
 mkdir -p /data/service/src
 wget -O /data/service/src/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war 
-mv  /data/service/src/jenkins.war  /data/service/tomcat/webapps
+
+#mv  /data/service/src/jenkins.war  /data/service/tomcat/webapps
 # 启动tomcat
 # 访问url
 
