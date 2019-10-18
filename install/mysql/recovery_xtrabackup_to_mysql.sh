2a9a6f1ea3146449b419cc4619b31e63050e54da
ehml79 <ehml79@foxmail.com>
2019-10-18 15:41:20 +0800

    add recovery_xtrabackup_to_mysql.sh

---
+++ install/mysql/recovery_xtrabackup_to_mysql.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# MySQL 5.7
+# Ubuntu 18.04 (bionic)
+
+TEMP_DIR=/data/temp
+
+mkdir -p ${TEMP_DIR}
+
+# 判断系统
+if [ -f /usr/bin/apt ];then
+    echo 'ubuntu'
+    sudo apt update &&
+    sudo dpkg -i https://www.percona.com/downloads/Percona-XtraBackup-2.4/Percona-XtraBackup-2.4.15/binary/debian/bionic/x86_64/percona-xtrabackup-24_2.4.15-1.bionic_amd64.deb
+elif [ -f /usr/bin/yum ];then
+    echo 'centOS'
+    yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
+    yum -y install percona-xtrabackup-24
+else
+    echo 'unknow OS'
+    exit 1
+fi
+
+tar xf ${1} -C ${TEMP_DIR}/
+
+# 恢复
+innobackupex --defaults-file=${TEMP_DIR}/backup-my.cnf --apply-log ${TEMP_DIR} 
+
+sed -i '/innodb_log_checksum_algorithm/d' ${TEMP_DIR}/backup-my.cnf
+sed -i '/innodb_fast_checksum/d' ${TEMP_DIR}/backup-my.cnf
+sed -i '/innodb_log_block_size/d' ${TEMP_DIR}/backup-my.cnf
+sed -i '/innodb_doublewrite_file/d' ${TEMP_DIR}/backup-my.cnf
+sed -i '/rds_encrypt_data/d' ${TEMP_DIR}/backup-my.cnf
+sed -i '/innodb_encrypt_algorithm/d' ${TEMP_DIR}/backup-my.cnf
+sed -i '/redo_log_version/d' ${TEMP_DIR}/backup-my.cnf
+sed -i '/master_key_id/d' ${TEMP_DIR}/backup-my.cnf
+sed -i '/server_uuid/d' ${TEMP_DIR}/backup-my.cnf
+echo "skip-grant-tables" >> ${TEMP_DIR}/backup-my.cnf 
+
+chown -R mysql:mysql  ${TEMP_DIR}
+
+/etc/init.d/mysql.server stop
+
+/data/service/mysql/bin/mysqld_safe --defaults-file=${TEMP_DIR}/backup-my.cnf --user=mysql --datadir=${TEMP_DIR} &
