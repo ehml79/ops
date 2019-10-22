2ea67cdcde1cf3f8cc5e86d5acda9785c727e4c6
ehml79 <ehml79@foxmail.com>
2019-10-22 22:59:48 +0800

    update

--- install/mysql/install_mysql-8.sh
+++ install/mysql/install_mysql-8.sh
@@ -2,38 +2,38 @@
 
 function install_mysql8(){
 
-mysql_version="mysql-8.0.15"
-mysql_passwd=`< /dev/urandom tr -dc A-Za-z0-9 | head -c16`
+    mysql_version="mysql-8.0.18"
+    mysql_passwd=`< /dev/urandom tr -dc A-Za-z0-9 | head -c16`
 
-sudo apt -y install make cmake gcc g++ bison libncurses5-dev build-essential libssl-dev  libaio1
-
-groupadd mysql
-useradd -r -g mysql -s /bin/false mysql
-
-mkdir -p /data/service/src/
-
-wget -O /data/service/src/${mysql_version}-linux-glibc2.12-x86_64.tar.xz  https://dev.mysql.com/get/Downloads/MySQL-8.0/${mysql_version}-linux-glibc2.12-x86_64.tar.xz 
-
-
-cd /data/service/src
-tar xf ${mysql_version}-linux-glibc2.12-x86_64.tar.xz 
-mv /data/service/src/${mysql_version}-linux-glibc2.12-x86_64 /data/service/mysql
-cd /data/service/mysql
-bin/mysqld --initialize-insecure --user=mysql  --basedir=/data/service/mysql --datadir=/data/service/mysql/data/
-
-# bin/mysqld_safe --user=mysql &
-# killall mysqld
-
-
-cp support-files/mysql.server /etc/init.d/mysqld
-sed -i 's@/usr/local/mysql@/data/service/mysql@g' /etc/init.d/mysqld
-systemctl enable mysqld
-/etc/init.d/mysqld start
-
-mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${mysql_passwd}';"
-
-export PATH=$PATH:/data/service/mysql/bin
-echo 'export PATH=$PATH:/data/service/mysql/bin' >> /etc/profile
+    sudo apt -y install make cmake gcc g++ bison libncurses5-dev build-essential libssl-dev  libaio1
+    
+    groupadd mysql
+    useradd -r -g mysql -s /bin/false mysql
+    
+    mkdir -p /data/service/src/
+    
+    wget -O /data/service/src/${mysql_version}-linux-glibc2.12-x86_64.tar.xz  https://dev.mysql.com/get/Downloads/MySQL-8.0/${mysql_version}-linux-glibc2.12-x86_64.tar.xz 
+    
+    
+    cd /data/service/src
+    tar xf ${mysql_version}-linux-glibc2.12-x86_64.tar.xz 
+    mv /data/service/src/${mysql_version}-linux-glibc2.12-x86_64 /data/service/mysql
+    cd /data/service/mysql
+    bin/mysqld --initialize-insecure --user=mysql  --basedir=/data/service/mysql --datadir=/data/service/mysql/data/
+    
+    # bin/mysqld_safe --user=mysql &
+    # killall mysqld
+    
+    
+    cp support-files/mysql.server /etc/init.d/mysqld
+    sed -i 's@/usr/local/mysql@/data/service/mysql@g' /etc/init.d/mysqld
+    systemctl enable mysqld
+    /etc/init.d/mysqld start
+    
+    mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${mysql_passwd}';"
+    
+    export PATH=$PATH:/data/service/mysql/bin
+    echo 'export PATH=$PATH:/data/service/mysql/bin' >> /etc/profile
 
 }
 
@@ -50,16 +50,16 @@ KexAlgorithms diffie-hellman-group1-sha1,curve25519-sha256@libssh.org,ecdh-sha2-
 Ciphers 3des-cbc,blowfish-cbc,aes128-cbc,aes128-ctr,aes256-ctr
 EOF
 
-/usr/bin/ssh-keygen -A
+    /usr/bin/ssh-keygen -A
 
-/usr/sbin/service ssh restart
+    /usr/sbin/service ssh restart
 
 
 }
 
 install_mysql8
 
-config_sshd
+# config_sshd
 
 
 rm /root/$0
