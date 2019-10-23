2979dddf703f825caf476304992a8a7a471196ab
ehml79 <ehml79@foxmail.com>
2019-10-24 00:26:32 +0800

    update

---
+++ install/python/pip-trusted-host.sh
@@ -0,0 +1,18 @@
+#!/bin/bas
+
+
+trusted_host="mirrors.aliyun.com"
+
+
+mkdir -p  /root/.pip
+
+cat > /root/.pip/pip.conf <<EOF
+## Note, this file is written by cloud-init on first boot of an instance
+## modifications made here will not survive a re-bundle.
+###
+[global]
+index-url=http://${trusted_host}/pypi/simple/
+
+[install]
+trusted-host=${trusted_host}
+EOF
