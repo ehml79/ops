0f3eacd898c62ae7ab16d01185333fbb70048cb5
ehml79 <ehml79@foxmail.com>
2019-10-30 20:36:30 +0800

    update

---
+++ install/elk/install_elasticsearch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# 
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.4.1-linux-x86_64.tar.gz
---
+++ install/elk/install_filebeat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.4.1-linux-x86_64.tar.gz
