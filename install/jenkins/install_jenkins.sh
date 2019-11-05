2d54c7ac2d14463082838efa1826657f2d3d6a83
ehml79 <ehml79@foxmail.com>
2019-11-05 18:17:54 +0800

    update

--- install/jenkins/install_jenkins.sh
+++ install/jenkins/install_jenkins.sh
@@ -7,10 +7,12 @@ apt -y  install openjdk-8-jdk-headless openjdk-8-jre-headless
 # install tomcat
 
 mkdir -p /data/service/src
-# 清华源
-#wget -O /data/service/jenkins.war https://mirrors.tuna.tsinghua.edu.cn/jenkins/war/latest/jenkins.war
+
 # 官方源
-wget -O /data/service/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+#wget -O /data/service/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+
+# 清华源
+wget -O /data/service/jenkins.war https://mirrors.tuna.tsinghua.edu.cn/jenkins/war/latest/jenkins.war
 
 # 启动jenkins
 mkdir -p /data/logs
