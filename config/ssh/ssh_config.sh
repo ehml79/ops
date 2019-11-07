76b1707d17fd392e5ec7eb055bcebf116bad20ed
ehml79 <ehml79@foxmail.com>
2019-11-07 14:18:33 +0800

    update

--- config/ssh/ssh_config.sh
+++ config/ssh/ssh_config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-HOST=vm2
-HOSTNAME=192.168.217.129
+HOST=vm3
+HOSTNAME=192.168.217.130
 
 cat >> ~/.ssh/config  << EOF
-Host vm2
-    Hostname 192.168.217.129
+Host ${HOST}
+    Hostname ${HOSTNAME}
     Port 22
     User root
     IdentityFile ~/.ssh/id_rsa
