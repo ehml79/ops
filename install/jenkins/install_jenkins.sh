6d99809e3f9edf382937a994223e9e654fdd085b
ehml79 <ehml79@foxmail.com>
2019-11-08 15:35:06 +0800

    update

--- install/jenkins/install_jenkins.sh
+++ install/jenkins/install_jenkins.sh
@@ -2,6 +2,8 @@
 
 # install java
 
+DOMAIN_NAME=jenkins.example.com
+
 apt -y  install openjdk-11-jdk-headless openjdk-11-jre-headless
 
 # install tomcat
@@ -17,7 +19,22 @@ wget -O /data/service/jenkins.war https://mirrors.tuna.tsinghua.edu.cn/jenkins/w
 # 启动jenkins
 mkdir -p /data/logs
 echo '#!/bin/bash'  > /root/jenkins_start.sh
-echo "nohup java -jar /data/service/jenkins.war --httpPort=80 > /data/logs/jenkins.log 2>&1 &" >> /root/jenkins_start.sh
+echo "nohup java -jar /data/service/jenkins.war --httpPort=8090 > /data/logs/jenkins.log 2>&1 &" >> /root/jenkins_start.sh
 
+
+
+cat > /data/service/nginx/conf/${DOMAIN_NAME}.conf <<EOF
+server {
+    listen       80;
+    server_name  ${DOMAIN_NAME};
+
+    location / {
+        proxy_pass http://127.0.0.1:8090;
+    }
+}
+EOF
+
+
+nginx -s reload
 
 rm  -fr /root/$0
