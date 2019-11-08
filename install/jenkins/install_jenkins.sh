d8dc6670348ca23d8e456a080e2ce021ee616a3e
ehml79 <ehml79@foxmail.com>
2019-11-08 16:40:55 +0800

    update

--- install/jenkins/install_jenkins.sh
+++ install/jenkins/install_jenkins.sh
@@ -11,10 +11,10 @@ apt -y  install openjdk-11-jdk-headless openjdk-11-jre-headless
 mkdir -p /data/service/src
 
 # 官方源
-#wget -O /data/service/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+wget -O /data/service/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 
 # 清华源
-wget -O /data/service/jenkins.war https://mirrors.tuna.tsinghua.edu.cn/jenkins/war/latest/jenkins.war
+#wget -O /data/service/jenkins.war https://mirrors.tuna.tsinghua.edu.cn/jenkins/war/latest/jenkins.war
 
 # 启动jenkins
 mkdir -p /data/logs
