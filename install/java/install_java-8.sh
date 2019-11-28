d07f3ddc2c4a10c9416379924fdf21bce2327e52
ehml79 <ehml79@foxmail.com>
2019-11-28 10:21:35 +0800

    update

--- install/java/install_java-8.sh
+++ install/java/install_java-8.sh
@@ -9,16 +9,16 @@ tar xf jdk-8u211-linux-x64.tar.gz -C /data/service/
 mv /data/service/jdk1.8.0_211 /data/service/jdk
 
 
-bash_file="/etc/profile"
+profiled="/etc/profile.d/java.sh"
 
 export JAVA_HOME=/data/service/jdk/             
 export JRE_HOME=${JAVA_HOME}/jre
 export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
 export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$PATH
 
-echo "export JAVA_HOME=/data/service/jdk/"  >>  ${bash_file}
-echo "export JRE_HOME=\${JAVA_HOME}/jre"  >> ${bash_file}
-echo "export CLASSPATH=.:\$JAVA_HOME/lib:\$JRE_HOME/lib:\$CLASSPATH"  >> ${bash_file}
-echo "export PATH=\$JAVA_HOME/bin:\$JRE_HOME/bin:\$PATH" >> ${bash_file}
+echo "export JAVA_HOME=/data/service/jdk/"  >  ${profiled}
+echo "export JRE_HOME=\${JAVA_HOME}/jre"  >> ${profiled}
+echo "export CLASSPATH=.:\$JAVA_HOME/lib:\$JRE_HOME/lib:\$CLASSPATH"  >> ${profiled}
+echo "export PATH=\$JAVA_HOME/bin:\$JRE_HOME/bin:\$PATH" >> ${profiled}
 
-source ${bash_file}
+source ${profiled}
