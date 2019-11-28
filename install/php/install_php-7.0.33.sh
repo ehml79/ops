de02968d3ebe2e29b9dd8ec4a4717d2b0d71f6a2
ehml79 <ehml79@foxmail.com>
2019-11-28 10:23:37 +0800

    update

--- install/php/install_php-7.0.33.sh
+++ install/php/install_php-7.0.33.sh
@@ -19,7 +19,7 @@ function install_openssl(){
     ln -s /data/service/openssl/lib/libssl.so.1.1 /usr/lib/libssl.so.1.1
     ln -s /data/service/openssl/lib/libcrypto.so.1.1 /usr/lib/libcrypto.so.1.1
 
-    echo 'export PATH=$PATH:/data/service/openssl/bin/' >>/etc/profile
+    echo 'export PATH=$PATH:/data/service/openssl/bin/' > /etc/profile.d/openssl.sh
 
 
 }
@@ -169,8 +169,8 @@ function install_php70(){
 
 
 
-    echo 'export PATH=$PATH:/data/service/php70/bin/' >>/etc/profile
-    echo 'export PATH=$PATH:/data/service/php70/sbin/' >>/etc/profile
+    echo 'export PATH=$PATH:/data/service/php70/bin/' >/etc/profile.d/php.sh
+    echo 'export PATH=$PATH:/data/service/php70/sbin/' >>/etc/profile.d/php.sh
 
     # mcrypt
     wget -O /data/service/src/mcrypt-1.0.1.tgz http://pecl.php.net/get/mcrypt-1.0.1.tgz 
