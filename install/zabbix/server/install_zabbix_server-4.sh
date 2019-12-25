ef2c390257507896b43e682c0524ecbb1322d1b6
ehml79 <ehml79@foxmail.com>
2019-12-25 23:59:16 +0800

    update

--- install/zabbix/server/install_zabbix_server-4.sh
+++ install/zabbix/server/install_zabbix_server-4.sh
@@ -74,7 +74,6 @@ DBHost=${zabbix_server_ip}
 DBName=zabbix
 DBUser=zabbix
 DBPassword=${zabbix_db_password} 
-DBSocket=/tmp/mysql.sock
 ListenIP=0.0.0.0
 StartPollersUnreachable=10
 StartTrappers=10
