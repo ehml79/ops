177c35b41239d6c4b6918b52f84c8722118f4f40
ehml79 <ehml79@foxmail.com>
2020-01-12 15:27:20 +0800

    update

--- install/nginx/install_nginx.sh
+++ install/nginx/install_nginx.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-NGINX="nginx-1.16.1"
 RUN_USER=nginx
+NGINX="nginx-1.16.1"
 
 INSTALL_DIR=/data/service
 SRC_DIR=${INSTALL_DIR}/src
 
 [ ! -d ${INSTALL_DIR} ] && mkdir -p ${INSTALL_DIR}
 [ ! -d ${SRC_DIR} ] && mkdir -p ${SRC_DIR}
-
 
 # Check if user is root
 if [ $(id -u) != "0" ]; then
@@ -22,19 +21,30 @@ install_nginx(){
     # 判断系统
     if [ -f /usr/bin/apt ];then
     	echo 'ubuntu'
-    	apt -y install  git libpcre3 libpcre3-dev  zlib1g-dev openssl libssl-dev  build-essential  python3-pip 
+    	apt -y install  git \
+        libpcre3 \
+        libpcre3-dev \
+        zlib1g-dev \
+        openssl \
+        libssl-dev \
+        build-essential \
+        python3-pip \
+        libgd-dev \
+        libgeoip-dev \
+        libgoogle-perftools-dev \
+        libatomic-ops-dev
     elif [ -f /usr/bin/yum ];then
     	echo 'centOS'
-    	yum -y install wget gcc-c++ git pcre-devel openssl-devel
+    	yum -y install wget \
+        gcc-c++ \
+        git \
+        pcre-devel \
+        openssl-devel
     else
     	echo 'unknow OS'
     	exit 1
     fi
     
-    # install uwsgi
-    pip3 install uwsgi
-   
-
     # install nginx
     cd ${SRC_DIR}
     [ ! -d /data/web ] && mkdir -p /data/web
@@ -87,103 +97,21 @@ install_nginx(){
     --with-pcre \
     --with-libatomic
 
-    
     make  && make install
 
     [ ! -d  ${INSTALL_DIR}/nginx/conf/vhost/  ] && mkdir -p ${INSTALL_DIR}/nginx/conf/{vhost,tcp,cert}
-    # 生成nginx.conf 配置文件
-    cat > ${INSTALL_DIR}/nginx/conf/nginx.conf << EOF
-#
-user ${RUN_USER} ${RUN_USER};
 
-worker_processes auto;
-#worker_cpu_affinity 00000001 00000010 00000100 00001000 00010000 00100000 01000000 10000000;
+    mv -f /root/nginx.conf ${INSTALL_DIR}/nginx/conf/
+    mv -f /root/fastcgi_sample.conf ${INSTALL_DIR}/nginx/conf/vhost/
 
-error_log  ${INSTALL_DIR}/nginx/logs/error.log  notice;
-
-pid        ${INSTALL_DIR}/nginx/logs/nginx.pid;
-
-worker_rlimit_nofile 65535;
-
-events {
-        use epoll;
-        worker_connections 65535;
-}
-
-# tcp
-
-#stream {
-#                proxy_connect_timeout 300s;
-#                proxy_timeout 300s;
-#                tcp_nodelay on;
-#                include stream/*.conf;
-#}
-
-
-http {
-
-        include       mime.types;
-        default_type  application/octet-stream;
-
-        log_format  main        '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                                '\$status \$body_bytes_sent "\$http_referer" '
-                                '"\$http_user_agent" \$http_x_forwarded_for "\$request_body"';
-
-        access_log  ${INSTALL_DIR}/nginx/logs/access.log  main;
-
-        charset  utf-8;
-        server_names_hash_bucket_size 128;
-        client_header_buffer_size 32k;
-        large_client_header_buffers 4 32k;
-        client_max_body_size 30m;
-        sendfile on;
-        tcp_nopush     on;
-        keepalive_timeout 60;
-        tcp_nodelay on;
-        server_tokens off;
-        client_body_buffer_size 512k;
-
-        #proxy_connect_timeout   5;
-        #proxy_send_timeout      60;
-        #proxy_read_timeout      5;
-        #proxy_buffer_size       16k;
-        #proxy_buffers           4 64k;
-        #proxy_busy_buffers_size 128k;
-        #proxy_temp_file_write_size 128k;
-
-        fastcgi_connect_timeout 300;
-        fastcgi_send_timeout 300;
-        fastcgi_read_timeout 300;
-        fastcgi_buffer_size 64k;
-        fastcgi_buffers 4 64k;
-        fastcgi_busy_buffers_size 128k;
-        fastcgi_temp_file_write_size 128k;
-
-        gzip on;
-        gzip_min_length  1k;
-        gzip_buffers     4 16k;
-        gzip_http_version 1.1;
-        gzip_comp_level 2;
-        gzip_types       text/plain application/x-javascript text/css application/xml;
-        gzip_vary on;
-
-
-        #limit_zone  crawler  \$binary_remote_addr  10m; server
-
-        server{
-                listen          80;
-                server_name     _;
-                return          404;
-        }
-
-        include vhost/*.conf;
+    echo "export PATH=\$PATH:${INSTALL_DIR}/nginx/sbin" > /etc/profile.d/nginx.sh
 
 }
-EOF
 
-mv /root/fastcgi_sample.conf ${INSTALL_DIR}/nginx/conf/vhost/
-mv /root/uwsgi_sample.conf ${INSTALL_DIR}/nginx/conf/vhost/
+config_uwsgi(){
 
+# install uwsgi
+pip3 install uwsgi
 
 # 生成 uwsgi 配置文件
     cat > ${INSTALL_DIR}/nginx/conf/uwsgi.ini <<EOF
@@ -235,42 +163,12 @@ post-buffering=4096
 
 EOF
 
-cat > ${INSTALL_DIR}/nginx/conf/fcgi.conf <<EOF
-fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
-fastcgi_param  SERVER_SOFTWARE    nginx;
-
-fastcgi_param  QUERY_STRING       \$query_string;
-fastcgi_param  REQUEST_METHOD     \$request_method;
-fastcgi_param  CONTENT_TYPE       \$content_type;
-fastcgi_param  CONTENT_LENGTH     \$content_length;
-
-fastcgi_param  SCRIPT_FILENAME    \$document_root\$fastcgi_script_name;
-fastcgi_param  SCRIPT_NAME        \$fastcgi_script_name;
-fastcgi_param  REQUEST_URI        \$request_uri;
-fastcgi_param  DOCUMENT_URI       \$document_uri;
-fastcgi_param  DOCUMENT_ROOT      \$document_root;
-fastcgi_param  SERVER_PROTOCOL    \$server_protocol;
-
-fastcgi_param  REMOTE_ADDR        \$remote_addr;
-fastcgi_param  REMOTE_PORT        \$remote_port;
-fastcgi_param  SERVER_ADDR        \$server_addr;
-fastcgi_param  SERVER_PORT        \$server_port;
-fastcgi_param  SERVER_NAME        \$server_name;
-
-# PHP only, required if PHP was built with --enable-force-cgi-redirect
-fastcgi_param  REDIRECT_STATUS    200;
-EOF
-
-
-    echo "export PATH=\$PATH:${INSTALL_DIR}/nginx/sbin" > /etc/profile.d/nginx.sh
 # 生成启动脚本
 cat > /root/uwsgi_restart.sh <<EOF
 
 #!/bin/bash
 
-
 pid_num=\`ps aux | grep "uwsgi.ini" | grep -v grep |awk '{print \$2}' | head -1\`
-
 
 if [ -n "\${pid_num}" ];then
     kill -9 \${pid_num}
@@ -283,8 +181,10 @@ fi
 uwsgi --py-auto-reload=1 --ini ${INSTALL_DIR}/nginx/conf/uwsgi.ini
 EOF
 
+mv -f /root/uwsgi_sample.conf ${INSTALL_DIR}/nginx/conf/vhost/
+
 }
 
-
-Install_Nginx
+config_uwsgi
+install_nginx
 
