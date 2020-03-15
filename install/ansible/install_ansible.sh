4f5115f885e6c5e2f959aa94e386d1efde258c30
ehml79 <ehml79@foxmail.com>
2020-03-15 18:06:56 +0800

    update install_ansible.sh

--- install/ansible/install_ansible.sh
+++ install/ansible/install_ansible.sh
@@ -4,7 +4,8 @@ apt -y install python3-pip
 
 pip3 install ansible
 
-
 mkdir /etc/ansible
 
 mv {ansible.cfg,hosts} /etc/ansible
+
+mkdir /root/ansible/roles/{defaults,handlers,tasks,templates,vars}
