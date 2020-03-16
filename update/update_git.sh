d20950eec2c1493ef866df082299403353147723
ehml79 <ehml79@foxmail.com>
2020-03-16 12:03:52 +0800

    add update_git.sh

---
+++ update/update_git.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+chown -R nginx.nginx /data/gittemp && chmod -R 775 /data/gittemp
+
+rsync_log=/data/logs/rsync_update_scripts.log
+#speed='--bwlimit=5000'
+progress='--progress'
+delete='--delete'
+
+relRsync="rsync -vzrtopg  ${speed} ${progress} --password-file=/etc/rsyncd/rsyncd.pass"
+
+cd /data/gittemp
+
+for CODE_DIR in  cdn  h5sdk  m  opd  p  pc  samplewww  sdk  web
+do
+  
+  # git clone -b master git@git.yuntkeji.com:/data/service/git/h5game.git
+  
+  cd /data/gittemp/${CODE_DIR}
+  #git checkout dev
+  git fetch --all
+  git reset --hard origin/master
+  git pull
+
+  # update cdn
+  if [ ${CODE_DIR} == "cdn" ];then
+    ${relRsync} --exclude="*.git" --exclude="*.apk" --exclude="*.log" /data/gittemp/${CODE_DIR} rsync@192.168.0.17::web  | tee -a  ${rsync_log}
+  else
+    # update sdk
+    ${relRsync} --exclude="*.git" --exclude="*.apk" --exclude="*.log" /data/gittemp/${CODE_DIR} rsync@192.168.0.19::web  | tee -a  ${rsync_log}
+  fi
+
+done
