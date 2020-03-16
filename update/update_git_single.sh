baf06446b087cfb15f0d2402c4e946af265b3bbd
ehml79 <ehml79@foxmail.com>
2020-03-16 16:48:54 +0800

    update update_git_single.sh

--- update/update_git_single.sh
+++ update/update_git_single.sh
@@ -2,11 +2,11 @@
 
 for dir in  dir1 dir2 dir3
 do 
-    echo ${dir}
-    cd  ${dir}
-    git fetch --all
-    git reset --hard origin/master
-    git pull
+  echo ${dir}
+  cd  ${dir}
+  git fetch --all
+  git reset --hard origin/master
+  git pull
 done
 
 chown -R www.www /data/gittemp/
