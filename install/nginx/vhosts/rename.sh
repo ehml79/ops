580e7890409ebba90109b9b4e7a5b0e6da900ffa
ehml79 <ehml79@foxmail.com>
2020-03-17 12:51:55 +0800

    update rename.sh

--- install/nginx/vhosts/rename.sh
+++ install/nginx/vhosts/rename.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # 批量更改域名
 
-old_domain=olddomain.com
+old_domain=old.com
 
-new_domain=domain.com
+new_domain=new.com
 
 vhost_dir=/data/service/nginx/conf/vhost
 
@@ -11,8 +11,8 @@ vhost_dir=/data/service/nginx/conf/vhost
 for old in $(ls ${vhost_dir})
 do
 	pre=$(echo ${old} | awk -F. '{print $1}')
-	mv ${vhost_dir}/${old} ${vhost_dir}/${pre}.${domain}.conf
-	echo ${pre}.${domain}.conf
+	mv ${vhost_dir}/${old} ${vhost_dir}/${pre}.${new_domain}.conf
+	echo ${pre}.${new_domain}.conf
 done
 
 
