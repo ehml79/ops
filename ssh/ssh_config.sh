f521d7b400b28e127b1272eadb163f220224190c
ehml79 <ehml79@foxmail.com>
2020-03-29 09:11:22 +0800

    update

---
+++ ssh/ssh_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+HOST=vm3
+HOSTNAME=192.168.217.130
+
+cat >> ~/.ssh/config  << EOF
+Host ${HOST}
+    Hostname ${HOSTNAME}
+    Port 22
+    User root
+    IdentityFile ~/.ssh/id_rsa
+    IdentitiesOnly yes
+
+EOF
