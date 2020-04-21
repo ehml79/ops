c111d2502ba10d7af78bd34e9aa1727b8c77f682
ehml79 <ehml79@foxmail.com>
2020-04-21 13:20:28 +0800

    update

--- redis/install_redis.sh
+++ redis/install_redis.sh
@@ -2,6 +2,14 @@
 
 REDIS_VERSION="redis-5.0.8"
 REDIS_PASSWD=`< /dev/urandom tr -dc A-Za-z0-9 | head -c16`
+REDIS_PORT=6379
+SRC_DIR=${SRC_DIR}
+REDIS_CONFIG_FILE=${REDIS_CONFIG_FILE}
+REDIS_DATA_DIR=/data/service/redis/data/${REDIS_PORT}
+
+REDIS_LOG_FILE=/data/service/redis/logs/redis_${REDIS_PORT}.log
+REDIS_EXECUTABLE=/data/service/redis/bin/redis-server
+CLI_EXEC=/data/service/redis/bin/redis-cli
 
 
 function install_redis(){
@@ -15,36 +23,50 @@ function install_redis(){
 	    exit 1
     fi
 
-    mkdir -p /data/service/src/
-    wget -O /data/service/src/${REDIS_VERSION}.tar.gz  http://download.redis.io/releases/${REDIS_VERSION}.tar.gz 
-    cd /data/service/src
+    mkdir -p ${SRC_DIR}/
+    wget -O ${SRC_DIR}/${REDIS_VERSION}.tar.gz  http://download.redis.io/releases/${REDIS_VERSION}.tar.gz 
+    cd ${SRC_DIR}
     tar xf ${REDIS_VERSION}.tar.gz
     cd ${REDIS_VERSION}/
     make 
     make install PREFIX=/data/service/redis 
-    mkdir /data/service/redis/{etc,rdb}
-    cp /data/service/src/${REDIS_VERSION}/redis.conf /data/service/redis/etc/
+    mkdir /data/service/redis/{etc,data,logs}
+    mkdir ${REDIS_DATA_DIR}
+
+    cp ${SRC_DIR}/${REDIS_VERSION}/${REDIS_PORT}.conf ${REDIS_CONFIG_FILE}
      
-    sed -i 's/^daemonize.*/daemonize yes/' /data/service/redis/etc/redis.conf
-    sed -i 's/^bind.*/bind 0.0.0.0/' /data/service/redis/etc/redis.conf
-    sed -i 's@^dir.*@dir /data/service/redis/rdb@' /data/service/redis/etc/redis.conf
+    sed -i "s#^port .\+#port ${REDIS_PORT}#" ${REDIS_CONFIG_FILE}
+    sed -i "s#^logfile .\+#logfile /data/service/redis/logs/redis_${REDIS_PORT}.log#" ${REDIS_CONFIG_FILE}
+    sed -i "s@^dir.*@dir /data/service/redis/data/${REDIS_PORT}@" ${REDIS_CONFIG_FILE}
+    sed -i "s#^pidfile .\+#pidfile /var/run/redis_${REDIS_PORT}.pid#" ${REDIS_CONFIG_FILE}
+    sed -i 's/^daemonize.*/daemonize yes/' ${REDIS_CONFIG_FILE}
+    sed -i 's/^bind.*/bind 0.0.0.0/' ${REDIS_CONFIG_FILE}
 
 
     # 设置密码
     if [ -n "${REDIS_PASSWD}" ];then
-        sed -i "s/^# requirepass foobared.*/requirepass ${REDIS_PASSWD}/" /data/service/redis/etc/redis.conf
+        sed -i "s/^# requirepass foobared.*/requirepass ${REDIS_PASSWD}/" ${REDIS_CONFIG_FILE}
     fi
 
-    # 启动redis
-    /data/service/redis/bin/redis-server /data/service/redis/etc/redis.conf
-    
-    
-    echo "/data/service/redis/bin/redis-server /data/service/redis/etc/redis.conf" > /root/redis_start.sh
-    echo "/data/service/redis/bin/redis-cli -a ${REDIS_PASSWD} shutdown" > /root/redis_stop.sh
     
     echo 'export PATH="$PATH:/data/service/redis/bin/"' > /etc/profile.d/redis.sh
 
+    mv redis_init_script /etc/init.d/redis_${REDIS_PORT}
+    
+    sed -i "s#^EXEC=.*#EXEC=${REDIS_EXECUTABLE}#" /etc/init.d/redis_${REDIS_PORT}
+    sed -i "s#^CLIEXEC=.*#CLIEXEC=${CLI_EXEC}#" /etc/init.d/redis_${REDIS_PORT}
+    sed -i "s#^PIDFILE=.*#PIDFILE=/var/run/redis_${REDIS_PORT}.pid#" /etc/init.d/redis_${REDIS_PORT}
+    sed -i "s#^CONF=.*#CONF="/data/service/redis/etc/${REDIS_PORT}.conf"#" /etc/init.d/redis_${REDIS_PORT}
+    sed -i "s#^REDISPORT=.*#REDISPORT="${REDIS_PORT}"#" /etc/init.d/redis_${REDIS_PORT}
 
+    if command -v update-rc.d >/dev/null 2>&1; then
+    	#if we're not a chkconfig box assume we're able to use update-rc.d
+    	update-rc.d redis_${REDIS_PORT} defaults && echo "Success!"
+    else
+    	echo "No supported init tool found."
+    fi
+
+    /etc/init.d/redis_${REDIS_PORT} start
 
 }
 
