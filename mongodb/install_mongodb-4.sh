02e343a53e98dcc580a5b87224561956653cc885
ehml79 <ehml79@foxmail.com>
2020-05-06 16:15:39 +0800

    update

--- mongodb/install_mongodb-4.sh
+++ mongodb/install_mongodb-4.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-VERSION=4.2.5
+MONGODB_VERSION=4.2.5
+MONGODB_PASSWORD=`< /dev/urandom tr -dc A-Za-z0-9 | head -c16`
 
 
 
@@ -30,11 +31,57 @@ function install_mongodb(){
         exit 1
     fi
  
-    mkdir -p /data/service/mongodb/data
+    mkdir -p /data/service/mongodb/{etc,data}
     touch /data/service/mongodb/mongodb.log
+
+
+cat > /data/service/mongodb/etc/mongod.conf << EOF
+# mongod.conf
+
+# for documentation of all options, see:
+#   http://docs.mongodb.org/manual/reference/configuration-options/
+
+# where to write logging data.
+systemLog:
+  destination: file
+  logAppend: true
+  path: /data/service/mongodb/mongodb.log
+
+# Where and how to store data.
+storage:
+  dbPath: /data/service/mongodb/data/
+  journal:
+    enabled: true
+#  engine:
+#  mmapv1:
+#  wiredTiger:
+
+# how the process runs
+processManagement:
+  fork: true  # fork and run in background
+  pidFilePath: /var/run/mongodb/mongod.pid
+
+# network interfaces
+net:
+  port: 27017
+  bindIp: 127.0.0.1
+  unixDomainSocket:
+    enabled: false
+
+security:
+  authorization: enabled
+
+#operationProfiling:
+#replication:
+#sharding:
+EOF
     
-    /data/service/mongodb/bin/mongod --dbpath=/data/service/mongodb/data/  --logpath=/data/service/mongodb/mongodb.log -logappend --bind_ip 0.0.0.0 -port=27017   --fork 
+    #/data/service/mongodb/bin/mongod --dbpath=/data/service/mongodb/data/  --logpath=/data/service/mongodb/mongodb.log --auth  -logappend --bind_ip 0.0.0.0 -port=27017   --fork 
+    /data/service/mongodb/bin/mongod  -f /data/service/mongodb/etc/mongod.conf  
+    echo /data/service/mongodb/bin/mongo 127.0.0.1/admin --eval \"db.createUser\(\{user:\'root\',pwd:\'$MONGODB_PASSWORD\',roles:[\'userAdminAnyDatabase\']\}\)\" | bash
     
+    echo $MONGODB_PASSWORD > /data/service/mongodb/etc/mongodb.secret
+
     echo 'export PATH=$PATH:/data/service/mongodb/bin/' > /etc/profile.d/mongodb.sh
     export PATH=$PATH:/data/service/mongodb/bin/
 }
