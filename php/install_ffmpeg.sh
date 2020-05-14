d4fb38b54a95794f8a8dbb44eb6cad932d5cf490
ehml79 <ehml79@foxmail.com>
2020-05-14 12:00:17 +0800

    update

--- php/install_ffmpeg.sh
+++ php/install_ffmpeg.sh
@@ -8,5 +8,11 @@ tar xf ffmpeg-4.2.2.tar.bz2
 cd ffmpeg-4.2.2
 ./configure --prefix=/data/service/ffmpeg --enable-shared --disable-static --disable-doc --enable-ffplay --disable-x86asm
 make && sudo make install
-echo "include /data/service/ffmpeg" >> /etc/ld.so.conf
+echo "include /data/service/ffmpeg/lib" > /etc/ld.so.conf.d/ffmpeg.conf
 /sbin/ldconfig
+
+
+echo 'export PATH=$PATH:/data/service/ffmpeg/bin' > /etc/profile.d/ffmpeg.sh
+echo 'export FFMPEG_HOME=/data/service/ffmpeg' >> /etc/profile.d/ffmpeg.sh
+echo 'export PATH=$PATH:$FFMPEG_HOME/bin'  >> /etc/profile.d/ffmpeg.sh
+echo 'export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/data/service/ffmpeg/lib/pkgconfig' >> /etc/profile.d/ffmpeg.sh
