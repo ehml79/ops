9131147d56b197c90400a28d3f85737c58c3a5da
ehml79 <ehml79@foxmail.com>
2020-10-07 11:41:26 +0800

    update

--- timedatectl.sh
+++ timedatectl.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 
-mv /usr/share/zoneinfo/Asia/Singapore /etc/localtime
-
-# timedatectl set-timezone Asia/Singapore
+timedatectl set-timezone Asia/Singapore
 
 timedatectl set-local-rtc 1
 
