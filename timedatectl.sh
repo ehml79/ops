f2ea39376b0bd6bbdc064daca32d8a7325d8dc67
ehml79 <ehml79@foxmail.com>
2020-10-07 11:20:22 +0800

    update

--- timedatectl.sh
+++ timedatectl.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-timedatectl set-timezone "Asia/Shanghai"
+
+mv /usr/share/zoneinfo/Asia/Singapore /etc/localtime
+
+# timedatectl set-timezone Asia/Singapore
+
 timedatectl set-local-rtc 1
+
+systemctl restart cron.service
