723f11a45c54c13f208d50406aabf0025a047c2f
ehml79 <ehml79@foxmail.com>
2020-11-29 12:18:53 +0800

    update

---
+++ git/update/update_git.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+GIT_TEMP=/data/gittemp/
+WEB_DIR=/data/web/
+BRANCH=master
+
+for CODE_DIR in $(ls ${GIT_TEMP})
+do
+
+  # git clone -b master git@git.sample.com:/data/service/git/h5game.git
+
+  cd ${GIT_TEMP}/${CODE_DIR}
+  #git checkout ${BRANCH}
+  git fetch --all
+  git reset --hard origin/${BRANCH}
+  git pull
+
+done
+
+chown -R nginx.nginx ${GIT_TEMP}
+chmod -R 750 ${GIT_TEMP}
+rsync -av --exclude="*.git" ${GIT_TEMP}   ${WEB_DIR} 
+
+# systemctl restart gunicorn.service
