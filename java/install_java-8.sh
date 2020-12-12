537adad91f94848d25f784204f49bfbe195f9c8a
ehml79 <ehml79@foxmail.com>
2020-12-12 14:56:10 +0800

    Update install_java-8.sh

--- java/install_java-8.sh
+++ java/install_java-8.sh
@@ -11,7 +11,7 @@ mv /data/service/jdk1.8.0_211 /data/service/jdk
 
 profiled="/etc/profile.d/java.sh"
 
-export JAVA_HOME=/data/service/jdk/             
+export JAVA_HOME=/data/service/jdk
 export JRE_HOME=${JAVA_HOME}/jre
 export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
 export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$PATH
