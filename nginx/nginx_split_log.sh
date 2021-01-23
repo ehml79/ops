055396fef0178d44547a076319a58d4009018970
ehml79 <ehml79@foxmail.com>
2021-01-23 11:01:10 +0800

    update

--- nginx/nginx_split_log.sh
+++ nginx/nginx_split_log.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# 59 23 * * * nginx_cut_log.sh
+# 59 23 * * * nginx_split_log.sh
 
 logPath=/data/service/nginx/logs
 dateFmt=$(date -d '-1 day' +%F-%H-%M-%S)
 
 
 if [ -d ${logPath} ];then
-    mv ${logPath}/access.log ${logPath}/access_${dateFmt}.log
+    mv ${logPath}/access.log ${logPath}/backup/access_${dateFmt}.log
     kill -USR1 $(cat /data/service/nginx/logs/nginx.pid)
 fi
 
+
+find /data/service/nginx/logs/backup/ -mtime +30 -exec rm -rf {} \;
