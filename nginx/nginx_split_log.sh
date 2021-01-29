372ede2a81e77afc73d57d5ac2ede63b5610d107
ehml79 <ehml79@foxmail.com>
2021-01-29 22:53:17 +0800

    update

--- nginx/nginx_split_log.sh
+++ nginx/nginx_split_log.sh
@@ -7,7 +7,7 @@ backupPath=/data/service/nginx/logs/backup
 dateFmt=$(date -d '-1 day' +%F-%H-%M-%S)
 
 
-if [ -d ${backupPath} ];then
+if [ ! -d ${backupPath} ];then
     mkdir -p ${backupPath}
 fi
 
@@ -15,7 +15,7 @@ fi
 if [ -d ${logPath} ];then
     mv ${logPath}/access.log ${backupPath}/access_${dateFmt}.log
 #    kill -USR1 $(cat /data/service/nginx/logs/nginx.pid)
-    /data/service/nginx/sbin/nginx -s reopen
+    /data/service/nginx/sbin/nginx -s reopen
 fi
 
 
