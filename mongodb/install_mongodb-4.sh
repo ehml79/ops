eca64c2e39094ad456d75ce784dbfb2b8974ea0b
ehml79 <ehml79@foxmail.com>
2021-02-05 14:20:12 +0800

    update

--- mongodb/install_mongodb-4.sh
+++ mongodb/install_mongodb-4.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# for Ubuntu 20.04 LTS
+# for Ubuntu 18.04 LTS
+
 MONGODB_VERSION=4.4.2
 MONGODB_PASSWORD=`< /dev/urandom tr -dc A-Za-z0-9 | head -c16`
 
@@ -8,24 +11,23 @@ MONGODB_PASSWORD=`< /dev/urandom tr -dc A-Za-z0-9 | head -c16`
 function install_mongodb(){
 
     mkdir -p /data/service/src/
+    cd /data/service/src
 
     # 判断系统
     if [ -f /usr/bin/apt ];then
-        echo 'ubuntu'
         # Ubuntu 18.04
+        echo 'ubuntu'
         sudo apt-get -y install libcurl4 openssl
         wget -O  /data/service/src/mongodb-linux-x86_64-ubuntu1804-${MONGODB_VERSION}.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-${MONGODB_VERSION}.tgz
-        cd /data/service/src
         tar xf mongodb-linux-x86_64-ubuntu1804-${MONGODB_VERSION}.tgz
         mv mongodb-linux-x86_64-ubuntu1804-${MONGODB_VERSION} /data/service/mongodb
 
         SYSTEM_DIR=/lib/systemd/system/mongod.service 
     elif [ -f /usr/bin/yum ];then
-        echo 'centOS'
         # centOS 7
+        echo 'centOS'
         sudo yum -y install libcurl openssl wget
         wget -O /data/service/src/mongodb-linux-x86_64-rhel70-4.4.2.tgz  https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-4.4.2.tgz
-        cd /data/service/src
         tar xf mongodb-linux-x86_64-rhel70-${MONGODB_VERSION}.tgz
         mv mongodb-linux-x86_64-rhel70-${MONGODB_VERSION} /data/service/mongodb
 
@@ -63,7 +65,6 @@ sudo systemctl start mongod
 
 
 #    echo /data/service/mongodb/bin/mongo 127.0.0.1/admin --eval \"db.createUser\(\{user:\'root\',pwd:\'$MONGODB_PASSWORD\',roles:[\'userAdminAnyDatabase\']\}\)\" | bash
-    
 #    echo $MONGODB_PASSWORD > /data/service/mongodb/etc/mongodb.secret
 
     echo 'export PATH=$PATH:/data/service/mongodb/bin/' > /etc/profile.d/mongodb.sh
@@ -74,4 +75,34 @@ sudo systemctl start mongod
 # mongo -u usernamd -p password --port 27017 --host localhost
 # mongo localhost:27017/dbname  -u username -p password
 
+function install_tools(){
+
+    VERSION=$(grep "VERSION_ID" /etc/os-release | cut -f 2 -d '=')
+    cd /data/service/src
+    
+    if [ "${VERSION}"=="20.04" ];then
+        # for Ubuntu 20.04 
+        echo "20.04"
+        wget -O /data/service/src/mongodb-database-tools-ubuntu2004-x86_64-100.3.0.tgz  https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2004-x86_64-100.3.0.tgz 
+        tar xf mongodb-database-tools-ubuntu2004-x86_64-100.3.0.tgz
+        mv mongodb-database-tools-ubuntu2004-x86_64-100.3.0 /data/service/mongodb-database-tools
+    elif [ "${VERSION}"=="18.04" ];then
+        # for Ubuntu 18.04 
+        echo "18.04"
+        wget -O /data/service/src/mongodb-database-tools-ubuntu1804-x86_64-100.3.0.tgz  https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu1804-x86_64-100.3.0.tgz
+        tar xf mongodb-database-tools-ubuntu1804-x86_64-100.3.0.tgz
+        mv mongodb-database-tools-ubuntu1804-x86_64-100.3.0 /data/service/mongodb-database-tools
+    else
+        echo "Unknow"
+        exit
+    fi
+
+
+    echo 'export PATH=$PATH:/data/service/mongodb-database-tools/bin' > /etc/profile.d/mongodb-database-tools.sh
+    export PATH=$PATH:/data/service/mongodb-database-tools/bin
+}
+
+
+
 install_mongodb
+install_tools
