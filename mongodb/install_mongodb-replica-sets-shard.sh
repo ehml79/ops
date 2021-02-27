3198c15277ac0ae289e5858fa35c7f87414f492f
ehml79 <ehml79@foxmail.com>
2021-02-27 17:57:20 +0800

    update

--- mongodb/install_mongodb-replica-sets-shard.sh
+++ mongodb/install_mongodb-replica-sets-shard.sh
@@ -5,7 +5,7 @@
 # CentOS Linux release 7.9.2009 (Core)
 
 
-# 192.168.1.147        |192.168.1.156       |192.168.1.219
+# 192.168.213.131        |192.168.213.132       |192.168.213.133
 # Shard11:27001 主节点  |Shard12:27001 副节点|Shard13:27001 仲裁点
 # Shard21:27002 仲裁点  |Shard22:27002 主节点|Shard32:27002 副节点
 # Shard31:27003 副节点  |Shard32:27003 仲裁点|Shard33:27003 主节点
@@ -14,46 +14,9 @@
 
 
 
-node1=192.168.1.147
-node2=192.168.1.156
-node3=192.168.1.219
-
-
-# 系统全局允许分配的最大文件句柄数:
-sysctl -w fs.file-max=2097152
-sysctl -w fs.nr_open=2097152
-echo 2097152 > /proc/sys/fs/nr_open
-# 允许当前会话/进程打开文件句柄数:
-ulimit -n 1048576
-# 修改 ‘fs.file-max’ 设置到 /etc/sysctl.conf 文件:
-echo 'fs.file-max = 1048576' >> /etc/sysctl.conf
-
-# 修改/etc/security/limits.conf 持久化设置允许用户/进程打开文件句柄数
-cat >> /etc/security/limits.conf <EOF
-* soft nofile 1048576
-* hard nofile 1048576
-* soft nproc 524288
-* hard nproc 524288
-EOF
-
-# TCP 协议栈网络参数
-# 并发连接 backlog 设置:
-sysctl -w net.core.somaxconn=32768
-sysctl -w net.ipv4.tcp_max_syn_backlog=16384
-sysctl -w net.core.netdev_max_backlog=16384
-# 可用知名端口范围:
-sysctl -w net.ipv4.ip_local_port_range=80 65535
-sysctl -w net.core.rmem_default=262144
-sysctl -w net.core.wmem_default=262144
-sysctl -w net.core.rmem_max=16777216
-sysctl -w net.core.wmem_max=16777216
-sysctl -w net.core.optmem_max=16777216
-sysctl -w net.ipv4.tcp_rmem='1024 4096 16777216'
-sysctl -w net.ipv4.tcp_wmem='1024 4096 16777216'
-# TCP 连接追踪设置（Centos7以下才有，以上版本则不用）:
-sysctl -w net.nf_conntrack_max=1000000
-sysctl -w net.netfilter.nf_conntrack_max=1000000
-sysctl -w net.netfilter.nf_conntrack_tcp_timeout_time_wait=30
+node1=192.168.213.131
+node2=192.168.213.132
+node3=192.168.213.133
 
 
 
@@ -184,100 +147,114 @@ port = 20000
 fork = true
 
 #监听的配置服务器,只能有1个或者3个 configs为配置服务器的副本集名字
-configdb = configs/192.168.1.147:21000,192.168.1.156:21000,192.168.1.219:21000
+configdb = configs/${node1}:21000,${node2}:21000,${node3}:21000
  
 #设置最大连接数
 maxConns = 20000
 EOF
 
 
+
+
+
+
+
+
+
 # 启动三台服务器的config server
-mongod -f /data/service/mongodb/conf/config.conf
+ssh ${node1} "mongod -f /data/service/mongodb/conf/config.conf"
+ssh ${node2} "mongod -f /data/service/mongodb/conf/config.conf"
+ssh ${node3} "mongod -f /data/service/mongodb/conf/config.conf"
 
 
 
-mongo --port 21000
+mongo --host ${node1} --port 21000
 
+/bin/echo 'rs.status()' |  /data/service/mongodb/bin/mongo  ${node1}:21000 --quiet
+
+
+cat > /data/service/mongodb/bin/mongo  ${node1}:21000 --quiet <<EOF
 config = {
     _id : "configs",
     members : [
-    {_id : 0, host : "192.168.1.147:21000" },
-    {_id : 1, host : "192.168.1.156:21000" },
-    {_id : 2, host : "192.168.1.219:21000" }
+    {_id : 0, host : "${node1}:21000" },
+    {_id : 1, host : "${node2}:21000" },
+    {_id : 2, host : "${node3}:21000" }
     ]
 }
 
+EOF
+
+echo "config = { _id : "configs", members : [ {_id : 0, host : "${node1}:21000" }, {_id : 1, host : "${node2}:21000" }, {_id : 2, host : "${node3}:21000" } ] } "  |  /data/service/mongodb/bin/mongo  ${node1}:21000 --quiet
 
 
-rs.initiate(config)
+
+echo "rs.initiate(config)"  |  /data/service/mongodb/bin/mongo  ${node1}:21000 --quiet
+
+
+
+
 
 
 
 
 
 # 启动三台服务器的shard1 server
-mongod -f /data/service/mongodb/conf/shard1.conf
+ssh ${node1} "mongod -f /data/service/mongodb/conf/shard1.conf"
+ssh ${node2} "mongod -f /data/service/mongodb/conf/shard1.conf"
+ssh ${node3} "mongod -f /data/service/mongodb/conf/shard1.conf"
 
 
 
-mongo --port 27001
 
-use admin
-
-
-config = {
-    _id : "shard1",
-     members : [
-         {_id : 0, host : "192.168.1.147:27001" },
-         {_id : 1, host : "192.168.1.156:27001" },
-         {_id : 2, host : "192.168.1.219:27001" , arbiterOnly: true }
-     ]
- }
+mongo --host ${node1} --port 27001
 
 
-rs.initiate(config)
+echo "config = { _id : "shard1", members : [ {_id : 0, host : "${node1}:27001" }, {_id : 1, host : "${node2}:27001" }, {_id : 2, host : "${node3}:27001" , arbiterOnly: true } ] } " |  /data/service/mongodb/bin/mongo  ${node1}:21000 --quiet
 
 
-rs.status()
+
+echo "rs.initiate(config)" |  /data/service/mongodb/bin/mongo ${node1}:27001 --quiet
+
+
+# rs.status()
+# rs.isMaster()
+
 
 
 # 启动三台服务器的shard2 server
-mongod -f /data/service/mongodb/conf/shard2.conf
+ssh ${node1} "mongod -f /data/service/mongodb/conf/shard2.conf"
+ssh ${node2} "mongod -f /data/service/mongodb/conf/shard2.conf"
+ssh ${node3} "mongod -f /data/service/mongodb/conf/shard2.conf"
 
-mongo --port 27002
+mongo --host ${node2}  27002
 
-use admin
 
-config = {
-    _id : "shard2",
-     members : [
-         {_id : 0, host : "192.168.1.147:27002" , arbiterOnly: true},
-         {_id : 1, host : "192.168.1.156:27002" },
-         {_id : 2, host : "192.168.1.219:27002" }
-     ]
- }
+
+echo "config = { _id : "shard2", members : [ {_id : 0, host : "${node1}:27002" , arbiterOnly: true}, {_id : 1, host : "${node2}:27002" }, {_id : 2, host : "${node3}:27002" } ] } " |  /data/service/mongodb/bin/mongo  ${node1}:21000 --quiet 
 
 rs.initiate(config)
 
 rs.status()
+
+
+
+
 
 
 
 # 启动三台服务器的shard3 server
-mongod -f /data/service/mongodb/conf/shard3.conf
+ssh ${node1} "mongod -f /data/service/mongodb/conf/shard3.conf"
+ssh ${node2} "mongod -f /data/service/mongodb/conf/shard3.conf"
+ssh ${node3} "mongod -f /data/service/mongodb/conf/shard3.conf"
 
-mongo --port 27003
 
-use admin
 
-config = {
-    _id : "shard3",
-     members : [
-         {_id : 0, host : "192.168.1.147:27003" },
-         {_id : 1, host : "192.168.1.156:27003" , arbiterOnly: true},
-         {_id : 2, host : "192.168.1.219:27003" }
-     ]
- }
+mongo --host ${node3}  27003
+
+
+
+echo "config = { _id : "shard3", members : [ {_id : 0, host : "${node1}:27003" , arbiterOnly: true}, {_id : 1, host : "${node2}:27003" }, {_id : 2, host : "${node3}:27003" } ] } " |  /data/service/mongodb/bin/mongo  ${node1}:21000 --quiet
 
 rs.initiate(config)
 
@@ -285,8 +262,16 @@ rs.status()
 
 
 
+
+
+
 # 启动三台服务器的mongos server
-mongos -f /data/service/mongodb/conf/mongos.conf
+ssh ${node1} "mongos -f /data/service/mongodb/conf/mongos.conf"
+ssh ${node2} "mongos -f /data/service/mongodb/conf/mongos.conf"
+ssh ${node3} "mongos -f /data/service/mongodb/conf/mongos.conf"
+
+
+
 
 mongo --port 20000
 
@@ -294,9 +279,9 @@ use  admin
 
 
 # 串联路由服务器与分配副本集
-sh.addShard("shard1/192.168.1.147:27001,192.168.1.156:27001,192.168.1.219:27001");
-sh.addShard("shard2/192.168.1.147:27002,192.168.1.156:27002,192.168.1.219:27002");
-sh.addShard("shard3/192.168.1.147:27003,192.168.1.156:27003,192.168.1.219:27003");
+sh.addShard("shard1/${node1}:27001,${node2}:27001,${node3}:27001");
+sh.addShard("shard2/${node1}:27002,${node2}:27002,${node3}:27002");
+sh.addShard("shard3/${node1}:27003,${node2}:27003,${node3}:27003");
 
 
 # 查看集群状态
