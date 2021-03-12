ed23d700de21fba7261e3c74ded9281adcb68386
ehml79 <ehml79@foxmail.com>
2021-03-12 10:47:37 +0800

    update

--- hosts_deny.sh
+++ hosts_deny.sh
@@ -4,7 +4,7 @@
 count=$(lastb | awk '{print $3}' | sort -rn | uniq -c |head -1 | awk '{print $1}')
 ip=$(lastb | awk '{print $3}' | sort -rn | uniq -c |head -1 | awk '{print $2}')
 
-if [ $count -gt 3 ];then
+if [ $count -gt 10 ];then
     echo $ip
     echo "sshd: ${ip}" >> /etc/hosts.deny
 fi
