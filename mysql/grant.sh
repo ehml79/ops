96bac2a1af5f846bbf2dec79810b0be30c0fde95
ehml79 <ehml79@foxmail.com>
2021-03-15 12:02:05 +0800

    update

--- mysql/grant.sh
+++ mysql/grant.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 
-
-
-
-CREATE DATABASE 'dbname' ;
+CREATE DATABASE dbname ;
 
 CREATE USER 'username'@'%' IDENTIFIED BY 'password';
 
 CREATE USER 'username'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 CREATE USER 'username'@'%' IDENTIFIED WITH caching_sha2_password BY 'password';
 
-GRANT ALL PRIVILEGES ON 'username'.* TO 'username'@'%' ;
+GRANT ALL PRIVILEGES ON username.* TO username@'%' ;
 
 ALTER USER 'username'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 ALTER USER 'username'@'%' IDENTIFIED WITH caching_sha2_password BY 'password';
