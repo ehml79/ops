9c185bf4096fb9502a5dc15d09e12d65a0b36cc0
ehml79 <ehml79@foxmail.com>
2021-05-18 11:20:37 +0800

    update

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-apt -y install python3-pip
+# 判断系统
+if [ -f /usr/bin/apt ];then
+    apt -y install python3-pip
+elif [ -f /usr/bin/yum ];then
+    yum -y install python3-pip
+else
+    echo 'unknow OS'
+    exit 1
+fi
 
-pip3 install ansible
+pip3 install --upgrade pip -i https://mirrors.aliyun.com/pypi/simple/
+
+pip3 install ansible  -i https://mirrors.aliyun.com/pypi/simple/
 
 mkdir /etc/ansible
 
