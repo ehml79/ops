b36aa17927e7c070258730bb50a852b29dbe1939
ehml79 <ehml79@foxmail.com>
2023-06-12 17:33:37 +0800

    update

--- mongodb/mongo_connetion.sh
+++ mongodb/mongo_connetion.sh
@@ -8,3 +8,7 @@ mongo -uuser -ppassword  1.1.1.1:27017/admin
 
 # mongo集群连接方式
 mongo mongodb://1.1.1.1:27017,2.2.2.2:27017/admin  -uuser -ppassword
+
+
+# mongo删库
+mongo dbname --host 1.1.1.1:27017 -authenticationDatabase admin -u username -p 'password' --eval 'db.dropDatabase();'
