d4e21e764cb0266cc0993eee0dec7f55b3fd9251
ehml79 <ehml79@foxmail.com>
2023-06-16 22:38:23 +0800

    update

--- python/install_anaconda.sh
+++ python/install_anaconda.sh
@@ -34,6 +34,24 @@ conda clean -i
 }
 
 
+function pip_trush(){
+mkdir -p  ~/.pip
+
+cat > ~/.pip/pip.conf <<EOF
+## Note, this file is written by cloud-init on first boot of an instance
+## modifications made here will not survive a re-bundle.
+###
+[global]
+index-url=https://mirrors.aliyun.com/pypi/simple/
+
+[install]
+trusted-host=mirrors.aliyun.com
+EOF
+
+}
+
 download_package
 
 set_anaconda
+
+pip_trush
